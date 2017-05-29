53752a865eaa8dd9f0e9cf5fed16bb17aaaf28c1
Macbook Pro 15 <freeaanzee@gmail.com>
2017-05-29 17:12:18 +0200

    Aantal argumenten expliciet meegeven

--- functions.php
+++ functions.php
@@ -404,8 +404,8 @@
 	add_filter( 'woocommerce_email_footer_text', 'do_shortcode' );
 	
 	// Pas het onderwerp van de mails aan naargelang de gekozen levermethode
-	// add_filter( 'woocommerce_email_subject_customer_processing_order', 'change_processing_order_subject' );
-	add_filter( 'woocommerce_email_subject_customer_completed_order', 'change_completed_order_subject' );
+	// add_filter( 'woocommerce_email_subject_customer_processing_order', 'change_processing_order_subject', 10, 2 );
+	add_filter( 'woocommerce_email_subject_customer_completed_order', 'change_completed_order_subject', 10, 2 );
 
 	function change_processing_order_subject( $subject, $order ) {
 		return $subject;
@@ -422,7 +422,7 @@
 	}
 
 	// Pas de header van de mails aan naargelang de gekozen levermethode
-	add_filter( 'woocommerce_email_heading_customer_completed_order', 'change_completed_email_heading' );
+	add_filter( 'woocommerce_email_heading_customer_completed_order', 'change_completed_email_heading', 10, 2 );
 
 	function change_completed_email_heading( $email_heading, $order ) {
 		if ( $order->has_shipping_method('local_pickup_plus') ) {
