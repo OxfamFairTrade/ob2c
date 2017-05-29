277abb7f1f7dc4307e90d84ce473cfdcc2e7c09c
Macbook Pro 15 <freeaanzee@gmail.com>
2017-05-29 17:21:28 +0200

    Toon productfotootjes in mails aan klanten

---
+++ woocommerce/emails/email-order-details.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Order details table shown in emails.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates/Emails
+ * @version     3.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$text_align = is_rtl() ? 'right' : 'left';
+
+do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
+
+<?php if ( ! $sent_to_admin ) : ?>
+	<!-- GEWIJZIGD: Geen hashtag voor bestelnummer -->	
+	<h2><?php printf( 'Bestelling %s', $order->get_order_number() ); ?></h2>
+<?php else : ?>
+	<!-- GEWIJZIGD: Geen hashtag voor bestelnummer -->	
+	<h2><a class="link" href="<?php echo esc_url( admin_url( 'post.php?post=' . $order->get_id() . '&action=edit' ) ); ?>"><?php printf( 'Bestelling %s', $order->get_order_number() ); ?></a> (<?php printf( '<time datetime="%s">%s</time>', $order->get_date_created()->format( 'c' ), wc_format_datetime( $order->get_date_created() ) ); ?>)</h2>
+<?php endif; ?>
+
+<table class="td" cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" border="1">
+	<thead>
+		<tr>
+			<th class="td" scope="col" style="text-align:<?php echo $text_align; ?>;"><?php _e( 'Product', 'woocommerce' ); ?></th>
+			<th class="td" scope="col" style="text-align:<?php echo $text_align; ?>;"><?php _e( 'Quantity', 'woocommerce' ); ?></th>
+			<th class="td" scope="col" style="text-align:<?php echo $text_align; ?>;"><?php _e( 'Price', 'woocommerce' ); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+		<!-- GEWIJZIGD: Fotootjes tonen aan klanten (met iets grotere thumbnail) en platte tekst uit -->
+		<?php echo wc_get_email_order_items( $order, array(
+			'show_sku'      => $sent_to_admin,
+			'show_image'    => !$sent_to_admin,
+			'image_size'    => array(48, 48),
+			'plain_text'    => false,
+			'sent_to_admin' => $sent_to_admin,
+		) ); ?>
+	</tbody>
+	<tfoot>
+		<?php
+			if ( $totals = $order->get_order_item_totals() ) {
+				$i = 0;
+				foreach ( $totals as $total ) {
+					$i++;
+					?><tr>
+						<th class="td" scope="row" colspan="2" style="text-align:<?php echo $text_align; ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo $total['label']; ?></th>
+						<td class="td" style="text-align:<?php echo $text_align; ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo $total['value']; ?></td>
+					</tr><?php
+				}
+			}
+		?>
+	</tfoot>
+</table>
+
+<?php do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
