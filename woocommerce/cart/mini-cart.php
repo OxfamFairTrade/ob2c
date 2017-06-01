babf3aa2898f7f98382ea3242b622c94e71c3c1a
Macbook Pro 13 <freeaanzee@gmail.com>
2017-06-01 10:40:12 +0200

    Leeggoed in mini-cart vereenvoudigen

--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -83,7 +83,8 @@ $nm_cart_empty_class_attr = ( WC()->cart->is_empty() ) ? ' class="nm-cart-panel-
                             ?>
                             
                             <?php echo $product_name; ?>
-                            <?php echo WC()->cart->get_item_data( $cart_item ); ?>
+                            <!-- GEWIJZIGD: Metadata zoals 'Hoort bij' niet tonen -->
+                            <?php echo // WC()->cart->get_item_data( $cart_item ); ?>
                             
                             <div class="nm-cart-panel-quantity-pricing">
                                 <?php if ( $_product->is_sold_individually() ) : ?>
