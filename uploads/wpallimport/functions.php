5930dba34cbf587e5ae1b4e8c676ae780f19eff8
Macbook Pro 13 <freeaanzee@gmail.com>
2017-06-08 00:11:13 +0200

    Extra importfuncties

--- uploads/wpallimport/functions.php
+++ uploads/wpallimport/functions.php
@@ -1,18 +1,18 @@
 <?php
-	function stitch_countries( $land1 ) {
-		$countries = '';
-		if ( $land1 !== '' ) {
-			$countries .= $land1;
+	function alter_brand( $brand ) {
+		if ( $brand == 'Oxfam Fairtrade' or $brand == 'EZA' ) {
+			$brand = 'Oxfam Fair Trade';
 		}
-		return $countries;
+		return $brand;
 	}
 
-	function translate_tax( $percentage ) {
-		$rate = 'standard-rate';
-		if ( $percentage == '6' ) {
-			$rate = 'reduced-rate';
+	function only_last_term( $string ) {
+		$terms = explode( ', ', $string );
+		foreach( $terms as $term ) {
+			$parts = explode( '->', $term );
+			$answer[] = $parts[count($parts)-1];
 		}
-		return $rate;
+		return implode( ', ', $answer );
 	}
 
 	function calc_content_per_kg_l( $stat, $ompak ) {
@@ -45,18 +45,20 @@
 	function get_eprice( $cp, $stat, $ompak, $unit ) {
 		$eprice = "/";
 		$noemer = calc_content_per_kg_l( $stat, $ompak );
-		if ( $calc > 0 ) {
+		if ( $noemer > 0 ) {
 			$teller = floatval(str_replace(',', '.', $cp));
-			if ( $teller > 0 and $noemer > 0 ) {
+			if ( $teller > 0 ) {
 				$calc = $teller / $noemer;
-				$eprice = "&euro; ".number_format( $calc, 2, ',', '.');
-				if ( $unit == 'L' ) {
-					$eprice .= " per liter";
-				} else {
-					$eprice .= " per kilogram";
-				}
+				$eprice = number_format( $calc, 2, ',', '.');
 			}
 		}
 		return $eprice;
+	}
+
+	function ditch_zeros( $value ) {
+		if ( $value === '0' or $value === '0,0' or $value === '0,000' ) {
+			$value = '';
+		}
+		return $value;
 	}
 ?>
