0e79cdb8bf0055ffc2437b80e0638973588f208a
Macbook Pro 13 <freeaanzee@gmail.com>
2017-06-18 23:36:41 +0200

    Nog meer automatisatie

--- reseller-api.php
+++ reseller-api.php
@@ -25,36 +25,37 @@
 
 	// Instantiate Mollie class
 	$mollie = new Mollie_Reseller( MOLLIE_PARTNER, MOLLIE_PROFILE, MOLLIE_APIKEY );
-	$partner_id_customer = '2842281';
+	
+	switch_to_blog( 10 );
 
 	try {
-		
 		// Parameters op te halen uit site
-		switch_to_blog( 10 );
-			// BIJ REGIOWERKINGEN KAN DIT AFWIJKEN (= NAAM REKENINGWINKEL)
-			$company = get_bloginfo('name');
-			$address = get_oxfam_shop_data('place');
-			$zip = get_oxfam_shop_data('zipcode');
-			$city = get_oxfam_shop_data('city');
-			$phone = get_oxfam_shop_data('telephone');
-			$email = get_bloginfo('admin_email');
-			$btw = str_replace( ' ', '', str_replace( '.', '', get_oxfam_shop_data('tax') ) );
-			$headquarter = get_oxfam_shop_data('headquarter');
-			$lines = explode( ', ', $headquarter, 2 );
-			$billing_address = trim($lines[0]);
-			$parts = explode( ' ', $lines[1], 2 );
-			$billing_zip = trim($parts[0]);
-			$billing_city = trim($parts[1]);
-			$iban = str_replace( ' ', '', get_oxfam_shop_data('account') );
-			$blog = get_bloginfo('name');
-			$url = get_bloginfo('url');
-		restore_current_blog();
-
+		// BIJ REGIOWERKINGEN KAN DIT AFWIJKEN (= NAAM REKENINGWINKEL)
+		$company = get_bloginfo('name');
+		$address = get_oxfam_shop_data('place');
+		$zip = get_oxfam_shop_data('zipcode');
+		$city = get_oxfam_shop_data('city');
+		// $phone = '32'.substr( get_oxfam_shop_data('telephone'), 1 );
+		$phone = '3292822463';
+		$email = get_bloginfo('admin_email');
+		$btw = str_replace( ' ', '', str_replace( '.', '', get_oxfam_shop_data('tax') ) );
+		$headquarter = get_oxfam_shop_data('headquarter');
+		$lines = explode( ', ', $headquarter, 2 );
+		$billing_address = trim($lines[0]);
+		$parts = explode( ' ', $lines[1], 2 );
+		$billing_zip = trim($parts[0]);
+		$billing_city = trim($parts[1]);
+		$iban = str_replace( ' ', '', get_oxfam_shop_data('account') );
+		$blog = get_bloginfo('name');
+		$url = get_bloginfo('url');
+		
 		// Parameters handmatig in te vullen
-		$login = 'owwbrugge';
-		$name = 'Wim Spanhove';
-		$representative = 'Wim Spanhove';
-		$bic = 'GEBABEBB';
+		$login = 'owwdepinte';
+		$name = 'Jos Dekeukeleire';
+		$representative = 'Jos Dekeukeleire';
+		$bic = 'AXABBE22';
+		// $bic = 'GEBABEBB';
+		// $bic = 'GKCCBEBB';
 
 		// Lijken toch niet noodzakelijk te zijn: 'bankaccount_bankname' => 'BNP Paribas Fortis', 'bankaccount_location' => 'Brussel'
 		$parameters = array( 
@@ -76,36 +77,61 @@
 			'bankaccount_iban' => $iban,
 			'bankaccount_bic' => $bic, 
 		);
-		
-		echo '<pre>'.var_export($parameters, true).'</pre>';
 
 		// UITSCHAKELEN INDIEN VOOR ECHT!
 		$parameters['testmode'] = 1;
-		$simplexml = $mollie->accountCreate( $login, $parameters );
+		$accountxml = $mollie->accountCreate( $login, $parameters );
 
-		unset($parameters['testmode']);
-		// $simplexml = $mollie->accountEditByPartnerId( $partner_id_customer, $parameters );
-
+		// $partner_id_customer = '3169994';
+		$edit_parameters = array( 
+			'registration_number' => str_replace( 'BE', '', $btw ),
+			'vat_number' => $btw,
+		);
+		// $accountxml = $mollie->accountEditByPartnerId( $partner_id_customer, $edit_parameters );
 	} catch (Mollie_Exception $e) {
 		die('An error occurred: '.$e->getMessage());
 	}
 
-	if ( $simplexml->resultcode == '10' ) {
-		echo "<p>".$simplexml->resultmessage."</p>";
-		echo "<p>Partner-ID: ".$simplexml->partner_id."</p>";
-		echo "<p>Wachtwoord: ".$simplexml->password."</p>";
-		echo "<p></p>";
+	if ( $accountxml->resultcode == '10' ) {
+		echo "<p>".$accountxml->resultmessage."</p>";
+		echo "<p>Partner-ID: ".$accountxml->partner_id."</p>";
+		echo "<p>Wachtwoord: ".$accountxml->password."</p>";
+		echo "<p>Telefoon: ".$phone."</p>";
 		
-		// $simplexml = $mollie->profileCreateByPartnerId( $partner_id_customer, $blog, $url, $email, $phone, 5499 );
+		$profilexml = $mollie->profileCreateByPartnerId( $accountxml->partner_id, $blog, $url, $email, $phone, 5499 );
+		if ( update_option( 'oxfam_mollie_partner_id', $accountxml->partner_id ) ) {
+			echo "<p>Partner-ID gewijzigd in webshop!</p>";
+		}
 
-		if ( $simplexml->resultcode == '10' ) {
-			echo "<p>".$simplexml->resultmessage."</p>";
-			echo "<p>LIVE API: ".$simplexml->profile->api_keys->live."</p>";
+		$user = get_user_by( 'email', $email );
+		if ( $user ) {
+			wp_set_password( $accountxml->password, $user->ID );
+			echo "<p>Wachtwoord gekopieerd naar lokale beheerder!</p>";
+		}
+
+		echo "<p></p>";
+
+		if ( $profilexml->resultcode == '10' ) {
+			echo "<p>".$profilexml->resultmessage."</p>";
+			echo "<p>LIVE API: ".$profilexml->profile->api_keys->live."</p>";
+			echo "<p>TEST API: ".$profilexml->profile->api_keys->test."</p>";
+			if ( update_option( 'mollie-payments-for-woocommerce_live_api_key', $profilexml->profile->api_keys->live ) ) {
+				echo "<p>Live API-key gewijzigd in webshop!</p>";
+				if ( update_option( 'mollie-payments-for-woocommerce_test_mode_enabled', 'no' ) ) {
+					echo "<p>Testbetalingen uitgeschakeld!</p>";
+				}
+			}
+			if ( update_option( 'mollie-payments-for-woocommerce_test_api_key', $profilexml->profile->api_keys->test ) ) {
+				echo "<p>Test API-key gewijzigd in webshop!</p>";
+			}
+
 			echo "<p></p>";
 		} else {
-			echo '<pre>'.var_export($simplexml, true).'</pre>';
+			echo '<pre>'.var_export($profilexml, true).'</pre>';
 		}
 	} else {
-		echo '<pre>'.var_export($simplexml, true).'</pre>';
+		echo '<pre>'.var_export($accountxml, true).'</pre>';
 	}
+
+	restore_current_blog();
 ?>
