47de64d96c75063114db2007bcd64c70b17061df
Mac Mini <freeaanzee@gmail.com>
2017-10-15 13:20:28 +0200

    Icoontjes toevoegen

--- functions.php
+++ functions.php
@@ -71,6 +71,8 @@
 		wp_enqueue_style( 'oxfam-webshop', get_stylesheet_uri(), array( 'nm-core' ) );
 		// In de languages map van het child theme zal dit niet werken (checkt enkel nl_NL.mo) maar fallback is de algemene languages map (inclusief textdomain)
 		load_child_theme_textdomain( 'oxfam-webshop', get_stylesheet_directory().'/languages' );
+		wp_register_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css' );
+		wp_enqueue_style( 'bootstrap' );  
 		wp_enqueue_script( 'jquery-ui-autocomplete' );
 		wp_enqueue_script( 'jquery-ui-datepicker' );
 		wp_register_style( 'jquery-ui', 'https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css' );
@@ -3654,10 +3656,11 @@
  		$msg = '<img src="'.get_stylesheet_directory_uri().'/images/pointer-levering.png">';
  		$msg .= '<h3 class="thuislevering">'.__( 'Blokje uitleg bij store selector op basis van postcode.', 'oxfam-webshop' ).'</h3><br>';
 		$msg .= '<p style="text-align: center;">';
-		// $msg .= '<form id="referrer" action="javascript:void(0);">';
-		$msg .= '<input type="text" class="" id="oxfam-zip-user" style="width: 160px; height: 40px; text-align: center;" autocomplete="on"> ';
-		$msg .= '<input type="submit" class="button" id="do_oxfam_redirect" value="Stuur mij door" style="width: 160px; height: 40px; position: relative; top: -1px;" disabled>';
-		// $msg .= '</form>';
+		$msg .= '<div class="input-group add-on">';
+		$msg .= '<input type="text" class="form-control" placeholder="Search" id="oxfam-zip-user" style="width: 160px; height: 40px; text-align: center;" autocomplete="off"> ';
+		$msg .= '<div class="input-group-btn"><button class="btn btn-default" type="submit" id="do_oxfam_redirect" disabled><i class="glyphicon glyphicon-search"></i></button></div>';
+		// <input type="submit" class="button" id="do_oxfam_redirect" value="Stuur mij door" style="width: 160px; height: 40px; position: relative; top: -1px;" disabled>';
+		$msg .= '</div>';
 		foreach ( $all_zips as $zip => $city ) {
 			if ( isset( $global_zips[$zip] ) ) {
 				$url = $global_zips[$zip];
--- header-part-logo.php
+++ header-part-logo.php
@@ -7,33 +7,34 @@
 	} else {
 		$logo_href = NM_THEME_URI . '/img/logo@2x.png';
 	}
-    
-    // Alternative logo
-    $has_alt_logo = false;    
-    if ( $nm_theme_options['alt_logo_config'] != '0' ) {
-        if ( isset( $nm_theme_options['alt_logo'] ) && strlen( $nm_theme_options['alt_logo']['url'] ) > 0 ) {
-            $has_alt_logo = true;
-            $alt_logo_href = ( is_ssl() ) ? str_replace( 'http://', 'https://', $nm_theme_options['alt_logo']['url'] ) : $nm_theme_options['alt_logo']['url'];
-        }
-    }
+	
+	// Alternative logo
+	$has_alt_logo = false;    
+	if ( $nm_theme_options['alt_logo_config'] != '0' ) {
+		if ( isset( $nm_theme_options['alt_logo'] ) && strlen( $nm_theme_options['alt_logo']['url'] ) > 0 ) {
+			$has_alt_logo = true;
+			$alt_logo_href = ( is_ssl() ) ? str_replace( 'http://', 'https://', $nm_theme_options['alt_logo']['url'] ) : $nm_theme_options['alt_logo']['url'];
+		}
+	}
 ?>
 
-    <div class="nm-header-logo">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-            <?php
-                // GEWIJZIGD: Vermeld logo en winkelnaam enkel op lokale sites
-                if ( ! is_main_site() ) {
-                ?>
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/oww-webshop-groen-60px.png" class="nm-logo" style="max-height: 60px;">
-                    <?php if ( $has_alt_logo ) : ?>
-                    <img src="<?php echo esc_url( $alt_logo_href ); ?>" class="nm-alt-logo" alt="<?php bloginfo( 'name' ); ?>">
-                    <?php endif; ?>
-                <?php
-                    echo '<div class="winkelnaam">Webshop<br>' . str_replace( 'Oxfam-Wereldwinkel ', '', get_company_name() ) . '</div>';
-                } else {
-                    echo '<img src="'.get_stylesheet_directory_uri().'/images/oww-webshop-wit-60px.png" class="nm-logo" style="max-height: 60px;">';
-                    echo '<div class="winkelnaam" style="color: white;">Altijd fair<br>Altijd open</div>';
-                }
-            ?>
-        </a>
-    </div>
+	<div class="nm-header-logo">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+			<?php
+				// GEWIJZIGD: Vermeld logo en winkelnaam enkel op lokale sites
+				if ( ! is_main_site() ) {
+				?>
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/oww-webshop-groen-60px.png" class="nm-logo" style="max-height: 60px;">
+					<?php if ( $has_alt_logo ) : ?>
+					<img src="<?php echo esc_url( $alt_logo_href ); ?>" class="nm-alt-logo" alt="<?php bloginfo( 'name' ); ?>">
+					<?php endif; ?>
+				<?php
+					echo '<div class="winkelnaam">Webshop<br>' . str_replace( 'Oxfam-Wereldwinkel ', '', get_company_name() ) . '</div>';
+				} else {
+					echo '<img src="'.get_stylesheet_directory_uri().'/images/oww-webshop-zwart.png" class="nm-logo" style="max-height: 60px;">';
+					echo '<div class="winkelnaam">Webshop</div>';
+					echo '<img src="'.get_stylesheet_directory_uri().'/images/tekstballon.png" class="nm-logo" style="float: right; max-height: 60px;">';
+				}
+			?>
+		</a>
+	</div>
