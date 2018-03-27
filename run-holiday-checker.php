2751c7920a0708eee62e53ef26f22a5caee9ee60
Mac Mini <freeaanzee@gmail.com>
2018-03-27 11:47:39 +0200

    Stel waarschuwingsbanner in als testbetalingen actief staan

--- run-holiday-checker.php
+++ run-holiday-checker.php
@@ -8,28 +8,27 @@
 		require_once '../../../wp-load.php';
 		
 		if ( isset( $_GET['import_key'] ) and $_GET['import_key'] === IMPORT_KEY ) {
-			// Sluit afgeschermde en gearchiveerde webshops uit
-			$sites = get_sites( array( 'site__not_in' => get_site_option('oxfam_blocked_sites'), 'public' => 1, ) );
+			// Sluit hoofdsite en gearchiveerde webshops uit
+			$sites = get_sites( array( 'site__not_in' => array(1), 'public' => 1, ) );
 
 			foreach ( $sites as $site ) {
 				switch_to_blog( $site->blog_id );
 
-					// Sluit hoofdsite uit
-					if ( ! is_main_site() ) {
-						global $default_holidays;
-						// Stel boodschap in (personaliseerbaar maken? eerste werkdag zoeken na vakantie?)
-						update_option( 'woocommerce_demo_store_notice', 'We zijn vandaag uitzonderlijk gesloten. Bestellingen worden opnieuw verwerkt vanaf de eerstvolgende openingsdag. De geschatte leverdatum houdt hiermee rekening.' );
+					if ( get_option( 'mollie-payments-for-woocommerce_test_mode_enabled' ) === 'yes' ) {
+						// Stel de waarschuwingsbanner in
+						update_option( 'woocommerce_demo_store_notice', 'Opgelet: deze webshop is nog niet actief! De betalingen staan in testmodus.' );
+						update_option( 'woocommerce_demo_store', 'yes' );
+					} else {
 						if ( in_array( date_i18n('Y-m-d'), get_option('oxfam_holidays') ) ) {
+							// Stel de afwezigheidsboodschap in
+							// PERSONALISEERBAAR MAKEN? EERSTE WERKDAG ZOEKEN NA VAKANTIE?
+							update_option( 'woocommerce_demo_store_notice', 'We zijn vandaag uitzonderlijk gesloten. Bestellingen worden opnieuw verwerkt vanaf de eerstvolgende openingsdag. De geschatte leverdatum houdt hiermee rekening.' );
 							if ( update_option( 'woocommerce_demo_store', 'yes' ) ) {
 								echo "Vakantiebanner van ".$site->blogname." geactiveerd!<br>";
-							} else {
-								echo "Vakantiebanner van ".$site->blogname." ongewijzigd!<br>";
 							}
 						} else {
 							if ( update_option( 'woocommerce_demo_store', 'no' ) ) {
 								echo "Vakantiebanner van ".$site->blogname." gedeactiveerd!<br>";
-							} else {
-								echo "Vakantiebanner van ".$site->blogname." ongewijzigd!<br>";
 							}
 						}
 					}
