aa1abbc9b233a41f548122dd14113e6de3558f1d
Mac Mini <info@fullstackahead.be>
2018-05-23 19:56:18 +0200

    $quantity lijkt altijd ingesteld te zijn

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -21,8 +21,6 @@ if ( is_b2b_customer() ) {
 	if ( $multiple < 1 ) {
 		$multiple = 1;
 	}
-} else {
-	$multiple = 1;
 }
 
 write_log('ADD TO CART '.$product->get_sku().': '.$quantity.' quantity - '.$multiple.' multiple');
@@ -30,7 +28,7 @@ write_log('ADD TO CART '.$product->get_sku().': '.$quantity.' quantity - '.$mult
 echo apply_filters( 'woocommerce_loop_add_to_cart_link',
 	sprintf( '<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="%s">%s</a>',
 		esc_url( $product->add_to_cart_url() ),
-		esc_attr( isset( $quantity ) ? $quantity : $multiple ),
+		esc_attr( isset( $multiple ) ? $multiple : $quantity ),
 		esc_attr( $product->get_id() ),
 		esc_attr( $product->get_sku() ),
 		esc_attr( isset( $class ) ? $class : 'button' ),
