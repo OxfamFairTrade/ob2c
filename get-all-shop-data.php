acaff1940c306c756a536ce8fb8b0927ce027662
Mac Pro <info@fullstackahead.be>
2018-10-09 17:04:27 +0200

    Maak het mogelijk om gegevens van specifieke node op te vragen

--- get-all-shop-data.php
+++ get-all-shop-data.php
@@ -15,25 +15,38 @@
 		// Nodig want op de main site worden de gewoon de gegevens van de CVBA geretoureneerd (ID 28 = Regio Leuven)
 		switch_to_blog( 28 );
 
-		global $wpdb;
-		// Vraag ID's en namen van alle gepubliceerde winkels op
-		$results = $wpdb->get_results( "SELECT nid, title FROM node WHERE type = 'sellpoint' AND status = 1", OBJECT );
-		
-		// Print de header die InDesign begrijpt
-		$header = array( 'naam', 'straat', 'stad', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag', 'email', 'rekeningnummer', 'btw', 'telefoon', 'fax' );
-		echo implode( ';', $header ).'<br>';
+		if ( isset( $_GET['node'] ) ) {
 
-		foreach ( $results as $sellpoint ) {
-			$office_hours = get_office_hours($sellpoint->nid);
-			$data[] = $sellpoint->title;
-			// Vraag steeds de 'raw'-info op (zonder webshopcorrecties) door als 3de parameter 'true' mee te geven 
-			$data[] = get_oxfam_shop_data( 'place', $sellpoint->nid, true );
-			$data[] = get_oxfam_shop_data( 'zipcode', $sellpoint->nid, true ).' '.get_oxfam_shop_data( 'city', $sellpoint->nid, true );
+			echo implode( ';', get_data_for_sellpoint( $_GET['node'] ) );
+
+		} else {
+
+			global $wpdb;
+			// Vraag ID's en namen van alle gepubliceerde winkels op
+			$results = $wpdb->get_results( "SELECT nid, title FROM node WHERE type = 'sellpoint' AND status = 1", OBJECT );
 			
+			// Print de header die InDesign begrijpt
+			$header = array( 'naam', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag', 'straat', 'stad', 'email', 'rekeningnummer', 'btw', 'telefoon', 'fax' );
+			echo implode( ';', $header ).'<br/>';
+
+			foreach ( $results as $sellpoint ) {
+				// Bij numerieke keys voegt array_merge() de elementen gewoon achteraan toe!
+				$data = array_merge( $sellpoint->title, get_data_for_sellpoint( $sellpoint->nid ) );
+				echo implode( ';', $data ).'<br/>';
+				unset($data);
+			}
+
+		}
+
+		function get_data_for_sellpoint( $node ) {
+			$data = array();
+			
+			$office_hours = get_office_hours($node);
 			foreach( $office_hours as $day ) {
 				if ( $day === false ) {
 					$data[] = 'Gesloten';
 				} else {
+					$parts = array();
 					foreach( $day as $hours ) {
 						$parts[] = implode( ' - ', $hours );
 					}
@@ -42,14 +55,16 @@
 				}
 			}
 			
-			$data[] = get_oxfam_shop_data( 'mail', $sellpoint->nid, true );
-			$data[] = get_oxfam_shop_data( 'account', $sellpoint->nid, true );
-			$data[] = get_oxfam_shop_data( 'tax', $sellpoint->nid, true );
-			$data[] = get_oxfam_shop_data( 'telephone', $sellpoint->nid, true );
-			$data[] = get_oxfam_shop_data( 'fax', $sellpoint->nid, true );
-			
-			echo implode( ';', $data ).'<br>';
-			unset($data);
+			// Vraag steeds de 'raw'-info op (zonder webshopcorrecties) door als 3de parameter 'true' mee te geven 
+			$data[] = get_oxfam_shop_data( 'place', $node, true );
+			$data[] = get_oxfam_shop_data( 'zipcode', $node, true ).' '.get_oxfam_shop_data( 'city', $node, true );
+			$data[] = get_oxfam_shop_data( 'mail', $node, true );
+			$data[] = get_oxfam_shop_data( 'account', $node, true );
+			$data[] = get_oxfam_shop_data( 'tax', $node, true );
+			$data[] = get_oxfam_shop_data( 'telephone', $node, true );
+			$data[] = get_oxfam_shop_data( 'fax', $node, true );
+
+			return $data;
 		}
 		
 	?>
