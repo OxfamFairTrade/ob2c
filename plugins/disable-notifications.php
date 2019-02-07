5187d50732ac1861ff65c2458b8948edfc2cbcbe
Mac Pro <info@fullstackahead.be>
2019-02-07 13:06:10 +0100

    Tweaks

--- plugins/disable-notifications.php
+++ plugins/disable-notifications.php
@@ -1,11 +1,11 @@
 <?php
 	/*
-	Plugin Name: Disable Changed Password Notifications To Admins
+	Plugin Name: Disable Changed Password Notifications
 	Description: Kan eventueel ook omgeleid worden m.b.v. nieuwe 'wp_password_change_notification_email'-filter (WP 4.9+).
 	*/
 
-	if ( ! function_exists( 'wp_password_change_notification' ) ) {
-		function wp_password_change_notification() {
+	if ( ! function_exists('wp_password_change_notification') ) {
+		function wp_password_change_notification( $user ) {
 			return;
 		}
 	}
