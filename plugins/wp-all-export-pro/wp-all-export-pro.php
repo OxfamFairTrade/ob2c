d0e428976cd14a671710151c3f14e786139e2138
Macbook Pro 15 <info@fullstackahead.be>
2019-03-06 00:26:10 +0100

    Upgrade naar 1.5.5

--- plugins/wp-all-export-pro/wp-all-export-pro.php
+++ plugins/wp-all-export-pro/wp-all-export-pro.php
@@ -3,7 +3,7 @@
 Plugin Name: WP All Export Pro
 Plugin URI: http://www.wpallimport.com/export/
 Description: Export any post type to a CSV or XML file. Edit the exported data, and then re-import it later using WP All Import.
-Version: 1.5.4
+Version: 1.5.5
 Author: Soflyy
 */
 
@@ -47,7 +47,7 @@ else {
 	 */
 	define('PMXE_PREFIX', 'pmxe_');
 
-	define('PMXE_VERSION', '1.5.4');
+	define('PMXE_VERSION', '1.5.5');
 
 	define('PMXE_EDITION', 'paid');
 
