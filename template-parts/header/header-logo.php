e401c80370ff9fe288e8fded89ff61c86f513072
Mac Pro <info@fullstackahead.be>
2020-03-10 12:05:20 +0100

    Gebruik SVG-logo's in header

--- template-parts/header/header-logo.php
+++ template-parts/header/header-logo.php
@@ -7,13 +7,13 @@
 	<a href="<?php echo esc_url( home_url('/') ); ?>">
 		<!-- GEWIJZIGD: Vermeld logo en winkelnaam enkel op lokale sites -->
 		<?php if ( ! is_main_site() ) : ?>
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/oww-webshop-groen-60px@2x.png" class="nm-logo" alt="<?php bloginfo('name'); ?>">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-green.svg" class="nm-logo" alt="<?php bloginfo('name'); ?>">
 			<?php if ( $alt_logo_url ) : ?>
 				<img src="<?php echo esc_url( $alt_logo_url ); ?>" class="nm-alt-logo" alt="<?php bloginfo('name'); ?>">
 			<?php endif; ?>
 			<div class="winkelnaam">Webshop<br/><?php echo str_replace( 'Oxfam-Wereldwinkel ', '', get_company_name() ); ?></div>
 		<?php else : ?>
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/oww-webshop-zwart-60px@2x.png" class="nm-logo" alt="<?php bloginfo('name'); ?>">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-black.svg" class="nm-logo" alt="<?php bloginfo('name'); ?>">
 			<div class="winkelnaam black">Webshop</div>
 		<?php endif; ?>
 	</a>
