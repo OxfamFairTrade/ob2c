d582db7fe5a8f518da4065413512b9e1fe539ee6
Macbook Pro 15 <info@fullstackahead.be>
2020-04-14 13:02:50 +0200

    Alt-tag invullen

--- header-part-logo.php
+++ header-part-logo.php
@@ -22,13 +22,13 @@
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
 			<!-- GEWIJZIGD: Vermeld logo en winkelnaam enkel op lokale sites -->
 			<?php if ( ! is_main_site() ) : ?>
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/oww-webshop-groen-60px@2x.png" class="nm-logo">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/oww-webshop-groen-60px@2x.png" class="nm-logo" alt="Logo van Oxfam-Wereldwinkels">
 				<?php if ( $has_alt_logo ) : ?>
 					<img src="<?php echo esc_url( $alt_logo_href ); ?>" class="nm-alt-logo" alt="<?php bloginfo( 'name' ); ?>">
 				<?php endif; ?>
 				<div class="winkelnaam">Webshop<br><?php echo str_replace( 'Oxfam-Wereldwinkel ', '', get_company_name() ); ?></div>
 			<?php else : ?>
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/oww-webshop-zwart-60px@2x.png" class="nm-logo">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/oww-webshop-zwart-60px@2x.png" class="nm-logo" alt="Logo van Oxfam-Wereldwinkels">
 				<div class="winkelnaam black">Webshop</div>
 			<?php endif; ?>
 		</a>
