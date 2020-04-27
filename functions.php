122c865a1501a4b0d0957eb8d6fa0d6466a546b0
Macbook Pro 15 <info@fullstackahead.be>
2020-04-27 17:27:49 +0200

    Uitzondering voor Gistel en Knokke

--- functions.php
+++ functions.php
@@ -5256,7 +5256,7 @@
 		$output = '';
 		$days = get_office_hours( $atts['node'], $atts['id'] );
 		// Kijk niet naar sluitingsdagen bij winkels waar we expliciete afhaaluren ingesteld hebben
-		$exceptions = array( 'dilbeek', 'hoogstraten', 'leuven', 'roeselare', 'brussel' );
+		$exceptions = array( 'dilbeek', 'hoogstraten', 'leuven', 'roeselare', 'brussel', 'brugge', 'knokke', 'gistel' );
 		if ( in_array( $atts['id'], $exceptions ) ) {
 			$holidays = array();
 		} else {
@@ -5306,20 +5306,11 @@
 		// Boodschap over afhaling op afspraak enkel toevoegen indien hele week gesloten
 		// Uitzondering voor Kortrijk
 		if ( strpos( $output, ' - ' ) === false and get_current_blog_id() !== 18 ) {
-			if ( get_current_blog_id() === 25 ) {
-				// Uitzondering voor Brugge
-				$text = 'Om de verspreiding van het coronavirus tegen te gaan, zijn al onze winkels momenteel gesloten. Afhalen kan enkel in de Cash & Carry <b>op dinsdag of vrijdag tussen 10 en 12 uur</b>.';
-				// Extra tekst in de mail
-				if ( ! is_checkout() ) {
-					$text .= '<br/>Opgelet: de poort is gesloten, bel aan bij de deur links. We nemen steeds de nodige hygiënische maatregelen. Alvast bedankt voor je begrip!';
-				}
+			$locations = get_option('woocommerce_pickup_locations');
+			if ( count( $locations ) > 1 ) {
+				$text = 'Om de verspreiding van het coronavirus tegen te gaan, zijn al onze winkels momenteel gesloten. Afhalen kan enkel nog <u>op afspraak</u>. Na het plaatsen van je bestelling contacteren we je om een tijdstip af te spreken.';
 			} else {
-				$locations = get_option('woocommerce_pickup_locations');
-				if ( count( $locations ) > 1 ) {
-					$text = 'Om de verspreiding van het coronavirus tegen te gaan, zijn al onze winkels momenteel gesloten. Afhalen kan enkel nog <u>op afspraak</u>. Na het plaatsen van je bestelling contacteren we je om een tijdstip af te spreken.';
-				} else {
-					$text = 'Om de verspreiding van het coronavirus tegen te gaan, is onze winkel momenteel gesloten. Afhalen kan enkel nog <u>op afspraak</u>. Na het plaatsen van je bestelling contacteren we je om een tijdstip af te spreken.';
-				}
+				$text = 'Om de verspreiding van het coronavirus tegen te gaan, is onze winkel momenteel gesloten. Afhalen kan enkel nog <u>op afspraak</u>. Na het plaatsen van je bestelling contacteren we je om een tijdstip af te spreken.';
 			}
 			$output = '<p class="corona-notice">'.$text.'</p>';
 		} else {
@@ -5327,6 +5318,11 @@
 				// Uitzondering voor Leuven-Centrum
 				$text = '<br/>Bel naar 016/22.01.35 als je voor de deur staat en je onze aandacht niet weet te vangen. Als het je echt niet lukt om tijdens bovenstaande uren/dagen te komen, laat het ons dan zo vlug mogelijk weten. Dan maken we graag met jou een afspraak op maat.';
 				$output .= '<p class="corona-notice">'.$text.'</p>';
+			} elseif ( $atts['id'] === 'brugge' ) {
+				// Extra tekst in de mail
+				if ( ! is_checkout() ) {
+					$text .= '<br/>Opgelet: de poort is gesloten, bel aan bij de deur links. We nemen steeds de nodige hygiënische maatregelen. Alvast bedankt voor je begrip!';
+				}
 			}
 
 			// Knip de eerste <br/> er weer af
