83e8d8e51bce2cb8025d16243b9ab76da4c0503f
Macbook Pro 15 <info@fullstackahead.be>
2020-08-21 17:46:41 +0200

    Enkel absolute links gebruiken

--- template-parts/header/header-oww.php
+++ template-parts/header/header-oww.php
@@ -1,5 +1,7 @@
 <?php
 	global $nm_globals, $nm_theme_options;
+	// Voorlopig volledig statisch, behalve de 'Producten'-link
+	// We duiden 'Producten' voorlopig ook steeds aan als huidige pagina
 ?>
 
 <div id="header" class="hidden-sm hidden-xs">
@@ -7,7 +9,7 @@
 		<div class="col-row md-display-flex">
 			<div class="col-md-3 md-align-self-center">
 				<div class="logo">
-					<a href="/">
+					<a href="https://www.oxfamwereldwinkels.be/">
 						<img src="https://www.oxfamwereldwinkels.be/wp-content/themes/oxfam/images/logo-green.svg" alt="Logo Oxfam-Wereldwinkels">
 					</a>
 				</div>
@@ -15,13 +17,13 @@
 			<div class="col-md-9 md-align-self-center">
 				<div class="topbar">
 					<div class="top-menu">
-						<ul id="menu-top-menu" class="menu"><li id="menu-item-1627" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1627"><a href="https://www.oxfamwereldwinkels.be/pers/">Voor pers</a></li>
-							<li id="menu-item-1506" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1506"><a href="https://www.oxfamwereldwinkels.be/oxfam-op-school/">Voor scholen</a></li>
-							<li id="menu-item-75" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-75"><a target="_blank" rel="noopener noreferrer" href="https://www.oxfamfairtrade.be/nl/">Voor bedrijven</a></li>
+						<ul id="menu-top-menu" class="menu"><li class="menu-item menu-item-type-taxonomy menu-item-object-category"><a href="https://www.oxfamwereldwinkels.be/pers/">Voor pers</a></li>
+							<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="https://www.oxfamwereldwinkels.be/oxfam-op-school/">Voor scholen</a></li>
+							<li class="menu-item menu-item-type-custom menu-item-object-custom"><a target="_blank" rel="noopener noreferrer" href="https://www.oxfamfairtrade.be/nl/">Voor bedrijven</a></li>
 						</ul>
 					</div>
 					<div class="top-search">
-						<form id="globalSearch" action="/" method="get">
+						<form id="globalSearch" action="https://www.oxfamwereldwinkels.be/" method="get">
 							<input type="text" name="s" placeholder="Zoeken">
 							<input type="submit" value="">
 						</form>
@@ -35,12 +37,12 @@
 					</div>
 				</div>
 				<div id="nav" class="nav">
-					<ul id="menu-main-menu" class="menu"><li id="menu-item-1587" class="menu-item menu-item-type-post_type_archive menu-item-object-chain menu-item-1587"><a href="https://www.oxfamwereldwinkels.be/expertise/">Eerlijke handel</a></li>
-						<li id="menu-item-1515" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1515"><a href="https://www.oxfamwereldwinkels.be/producten/">Producten</a></li>
-						<li id="menu-item-1292" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1292"><a href="https://www.oxfamwereldwinkels.be/doe-mee/">Doe mee</a></li>
-						<li id="menu-item-135" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-135"><a href="https://www.oxfamwereldwinkels.be/over-ons/">Over ons</a></li>
-						<li id="menu-item-70" class="orange-btn menu-item menu-item-type-custom menu-item-object-custom menu-item-70"><a href="/winkels/">Vind winkel</a></li>
-						<li id="menu-item-71" class="green-btn menu-item menu-item-type-custom menu-item-object-custom menu-item-71"><a href="/word-vrijwilliger/">Word vrijwilliger</a></li>
+					<ul id="menu-main-menu" class="menu"><li class="menu-item menu-item-type-post_type_archive menu-item-object-chain menu-item-1587"><a href="https://www.oxfamwereldwinkels.be/expertise/">Eerlijke handel</a></li>
+						<li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item"><a href="<?php echo get_permalink( wc_get_page_id('shop') ); ?>">Producten</a></li>
+						<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="https://www.oxfamwereldwinkels.be/doe-mee/">Doe mee</a></li>
+						<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="https://www.oxfamwereldwinkels.be/over-ons/">Over ons</a></li>
+						<li class="orange-btn menu-item menu-item-type-custom menu-item-object-custom"><a href="https://www.oxfamwereldwinkels.be/winkels/">Vind winkel</a></li>
+						<li class="green-btn menu-item menu-item-type-custom menu-item-object-custom"><a href="https://www.oxfamwereldwinkels.be/word-vrijwilliger/">Word vrijwilliger</a></li>
 					</ul>
 				</div>
 				<div id="mobile-nav"></div>
@@ -53,13 +55,13 @@
 		<div class="col-row display-flex">
 			<div class="col-xs-6 align-self-center">
 				<div class="logo">
-					<a href="/"><img src="https://www.oxfamwereldwinkels.be/wp-content/themes/oxfam/images/logo-green.svg" alt=""></a>
+					<a href="https://www.oxfamwereldwinkels.be/"><img src="https://www.oxfamwereldwinkels.be/wp-content/themes/oxfam/images/logo-green.svg" alt=""></a>
 				</div>
 			</div> 
 			<div class="col-xs-6 align-self-center">
 				<div class="header-items display-flex">
 					<div class="header__item header__item_winkel">
-						<a href="/winkels/">Winkels</a>
+						<a href="https://www.oxfamwereldwinkels.be/winkels/">Winkels</a>
 					</div>
 					<div class="header__item header__item_search">
 						<span>Zoek</span>
@@ -70,7 +72,7 @@
 		<div id="menu-btn"><img src="https://www.oxfamwereldwinkels.be/wp-content/themes/oxfam/images/menu-btn-black.png" alt=""></div>
 	</div>
 	<div class="top-search top-search_mobile hidden">         
-		<form action="/" method="get">
+		<form action="https://www.oxfamwereldwinkels.be/" method="get">
 			<input type="text" name="s" placeholder="Zoeken">
 			<input type="submit" value="">
 		</form>
