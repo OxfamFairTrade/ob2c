b614159863f020be80cf571386c36b7624961106
Macbook Pro 15 <info@fullstackahead.be>
2020-08-27 19:27:20 +0200

    Pas volgorde labels aan

--- template-parts/woocommerce/product-labels.php
+++ template-parts/woocommerce/product-labels.php
@@ -2,6 +2,40 @@
 	global $product;
 	$labels = array();
 
+	if ( ! is_b2b_customer() and $product->is_on_sale() ) {
+		// Neem algemeen label als default
+		$labels['promotion'] = 'Promo';
+
+		// Zoek vervolgens de details van de actie op
+		// Handmatig beheerde lijst, want simpeler dan afleiden uit de kortingsregels!
+		
+		$one_plus_one_products = array();
+		if ( in_array( $product->get_sku(), $one_plus_one_products ) ) {
+			$labels['promotion'] = 'Promo 1+1 gratis';
+		}
+		
+		$fifty_percent_off_second_products = array( '21052', '24532', '25404', '25405', '25406', '27151' );
+		if ( in_array( $product->get_sku(), $fifty_percent_off_second_products ) ) {
+			$labels['promotion'] = 'Promo 2de -50%';
+		}
+		
+		$two_plus_one_products = array( '24102', '24117' );
+		if ( in_array( $product->get_sku(), $two_plus_one_products ) ) {
+			$labels['promotion'] = 'Promo 2+1 gratis';
+		}
+		
+		// Ons testproduct
+		$three_plus_one_products = array( '26424' );
+		if ( in_array( $product->get_sku(), $three_plus_one_products ) ) {
+			$labels['promotion'] = 'Promo 3+1 gratis';
+		}
+		
+		$twentyfive_percent_off_products = array();
+		if ( in_array( $product->get_sku(), $twentyfive_percent_off_products ) ) {
+			$labels['promotion'] = 'Promo -25%';
+		}
+	}
+
 	if ( $product->get_date_created()->date_i18n('Y-m-d') > date_i18n( 'Y-m-d', strtotime('-3 months') ) ) {
 		$labels['newbee'] = 'Nieuw';
 	}
@@ -11,50 +45,9 @@
 		$labels['organic'] = 'Bioproduct';
 	}
 
-	if ( ! is_b2b_customer() and has_term( 'promotie', 'product_tag', $product->get_id() ) ) {
-		$labels['promotion'] = 'Promo';
-	}
-
 	if ( ! does_risky_delivery() ) {
 		if ( $product->get_shipping_class() === 'breekbaar' ) {
 			$labels['pickup-only'] = 'Afhaling';
-		}
-	}
-
-	// Indien promo reeds actief: toon ook de details van de actie
-	// We beheren deze lijst handmatig, omdat het simpeler is dan het proberen af te leiden uit de kortingsregels
-	if ( array_key_exists( 'promotion', $labels ) ) {
-		$previous_count = count( $labels );
-
-		$one_plus_one_products = array();
-		if ( in_array( $product->get_sku(), $one_plus_one_products ) ) {
-			$labels['promotion one-plus-one'] = 'Promo 1+1 gratis';
-		}
-		
-		$fifty_percent_off_second_products = array( '21052', '24532', '25404', '25405', '25406', '27151' );
-		if ( in_array( $product->get_sku(), $fifty_percent_off_second_products ) ) {
-			$labels['promotion fifty-percent-off'] = 'Promo 2de -50%';
-		}
-		
-		$two_plus_one_products = array( '24102', '24117' );
-		if ( in_array( $product->get_sku(), $two_plus_one_products ) ) {
-			$labels['promotion two-plus-one'] = 'Promo 2+1 gratis';
-		}
-		
-		// Ons testproduct
-		$three_plus_one_products = array( '26424' );
-		if ( in_array( $product->get_sku(), $three_plus_one_products ) ) {
-			$labels['promotion three-plus-one'] = 'Promo 3+1 gratis';
-		}
-		
-		$twentyfive_percent_off_products = array();
-		if ( in_array( $product->get_sku(), $twentyfive_percent_off_products ) ) {
-			$labels['promotion twenty-five-percent-off'] = 'Promo -25%';
-		}
-
-		if ( count( $labels ) > $previous_count ) {
-			// De promo werd gespecifieerd, verwijder het algemene label
-			unset( $labels['promotion'] );
 		}
 	}
 
