9f8b20d52929cdb47c0356f5a06a5ec9de5e9cb7
Pieter Mathys <mathysp@gmail.com>
2020-09-04 16:53:28 +0200

    Add styling local-homepage templatet & fine-tune styling breadcrumbs, product lists

--- local-homepage.php
+++ local-homepage.php
@@ -18,57 +18,59 @@
 				<div class="breadcrumb">
 					<a href="https://stage.oxfamwereldwinkels.be/">Home</a> <span class="sep"></span> <span class="breadcrumb_last" aria-current="page">Webshop <?php echo get_webshop_name(); ?></span>
 				</div>
-				<div class="row">
-					<div class="col-md-6">
+				<div class="col-row lh-intro">
+					<div class="col-xs-12 col-md-6">
 						<h2>Oxfam Webshop <?php echo get_webshop_name(); ?></h2>
 						<?php the_content(); ?>
 					</div>
-					<div class="col-md-6">
+					<div class="col-xs-12 col-md-6">
 						<?php wc_get_template( 'product-searchform_nm.php' ); ?>
 					</div>
 					<?php get_template_part( 'template-parts/header/general-store-notice' ); ?>
 				</div>
-				<div class="row">
-					<div class="col-md-6">
+				<div class="col-row lh-header">
+					<div class="col-xs-12 col-sm-6">
 						<h2>Shoppen per categorie</h2>
 					</div>
-					<div class="col-md-6">
+					<div class="col-xs-12 col-sm-6">
 						<a href="#">Alle producten</a>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-12">
+				<div class="col-row lh-category">
+					<div class="col-xs-12">
 						<?php echo do_shortcode('[nm_product_categories orderby="term_order" title_tag="h3" parent="0"]'); ?>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-6">
+				<div class="col-row lh-header">
+					<div class="col-xs-12 col-sm-6">
 						<h2>Producten in de kijker</h2>
 					</div>
-					<div class="col-md-6">
+					<div class="col-xs-12 col-sm-6">
 						<a href="#">Alle producten</a>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-12">
+				<div class="col-row lh-featured">
+					<div class="col-xs-12">
 						<?php echo do_shortcode('[nm_product_slider shortcode="featured_products" per_page="-1" columns="4" columns_mobile="1" orderby="menu_order" order="ASC" arrows="1"]'); ?>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<div class="full-size-banner" style="background-image: url('<?php echo get_the_post_thumbnail_url( get_the_ID(), 'full' ); ?>'); background-size: cover; width: 100%; height: 400px;"></div>
+				<div class="col-row lh-banner">
+					<div class="col-xs-12">
+						<div class="full-size-banner">
+                            <img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'full' ); ?>" />
+                        </div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-6">
+				<div class="col-row lh-header">
+					<div class="col-xs-12 col-sm-6">
 						<h2>Promoties</h2>
 					</div>
-					<div class="col-md-6">
+					<div class="col-xs-12 col-sm-6">
 						<a href="#">Alle producten</a>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-12">
+				<div class="col-row lh-promo">
+					<div class="col-xs-12">
 						<?php echo do_shortcode('[nm_product_slider shortcode="sale_products" per_page="-1" columns="4" columns_mobile="1" orderby="menu_order" order="ASC" arrows="1"]'); ?>
 					</div>
 				</div>
