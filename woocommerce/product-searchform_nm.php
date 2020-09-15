6fdd1ae46deb02bf9b874abae9615b4af2382af4
Macbook Pro 15 <info@fullstackahead.be>
2020-09-15 15:38:01 +0200

    Huidige zoekterm altijd invullen

--- woocommerce/product-searchform_nm.php
+++ woocommerce/product-searchform_nm.php
@@ -1,8 +1,8 @@
 <div class="nm-shop-search-inner" style="border: 1px solid grey; padding: 1em; margin: 1em 0;">
     <div class="nm-shop-search-input-wrap">
         <a href="#" id="nm-shop-search-close"><i class="nm-font nm-font-close2"></i></a>
-        <form id="nm-shop-search-form" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-            <input type="text" id="nm-shop-search-input" autocomplete="off" value="" name="s" placeholder="<?php esc_attr_e( 'Search products', 'woocommerce' ); ?>" />
+        <form id="nm-shop-search-form" role="search" method="get" action="<?php echo esc_url( home_url('/') ); ?>">
+            <input type="text" id="nm-shop-search-input" autocomplete="off" value="<?php the_search_query(); ?>" name="s" placeholder="<?php esc_attr_e( 'Search products', 'woocommerce' ); ?>" />
             <input type="hidden" name="post_type" value="product" />
         </form>
     </div>
