de8fcf94f1fa19e9d74eb96302e900742e8d6a25
Mac Pro <info@fullstackahead.be>
2020-10-06 18:32:24 +0200

    Zoekopdracht verwijderbaar maken

--- woocommerce/product-searchform_nm.php
+++ woocommerce/product-searchform_nm.php
@@ -1,4 +1,4 @@
-<div class="nm-shop-search-inner" style="border: 1px solid grey; padding: 1em; margin: 1em 0;">
+<div class="nm-shop-search-inner">
     <div class="nm-shop-search-input-wrap">
         <a href="#" id="nm-shop-search-close"><i class="nm-font nm-font-close2"></i></a>
         <form id="nm-shop-search-form" role="search" method="get" action="<?php echo esc_url( home_url('/') ); ?>">
