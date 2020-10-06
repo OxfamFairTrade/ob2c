0ac89079a429bf45640c247e7c01572cb3c30f22
Mac Pro <info@fullstackahead.be>
2020-10-07 01:09:06 +0200

    'Uitverkocht' => 'Niet in assortiment'

--- update-stock-list.php
+++ update-stock-list.php
@@ -7,7 +7,7 @@
 
 	<p>Vink een product aan om het op de homepage te plaatsen of selecteer de juiste voorraadstatus om het in of uit de online verkoop te halen. Je aanpassing wordt onmiddellijk opgeslagen! Met de knop onderaan de pagina kun je alle producten in één keer in/uit voorraad halen. Een bevestigingsvenster behoedt je daarbij voor onbedoelde wijzigingen. <b>Tip: met Ctrl+F kun je snel zoeken naar een product.</b></p>
 
-	<p>Nieuwe producten, die in de loop van de voorbije 3 maanden beschikbaar werden op BestelWeb, hebben <span style="background-color: lightskyblue;">een blauwe achtergrond</span>. Ze verschijnen aanvankelijk als 'uitverkocht' in jullie lokale webshop, zodat je alle tijd hebt om te beslissen of je het product zal inkopen en online wil aanbieden. Producten die momenteel onbeschikbaar zijn op BestelWeb krijgen <span style="background-color: gold;">een gele achtergrond</span>, zodat het duidelijk is dat dit product misschien op zijn laatste benen loopt.</p>
+	<p>Nieuwe producten, die in de loop van de voorbije 3 maanden beschikbaar werden op BestelWeb, hebben <span style="background-color: lightskyblue;">een blauwe achtergrond</span>. Ze verschijnen aanvankelijk als 'niet in assortiment' in jullie lokale webshop, zodat je alle tijd hebt om te beslissen of je het product zal inkopen en online wil aanbieden. Producten die momenteel onbeschikbaar zijn op BestelWeb krijgen <span style="background-color: gold;">een gele achtergrond</span>, zodat het duidelijk is dat dit product misschien op zijn laatste benen loopt.</p>
 
 	<p>Oude producten die definitief niet meer te bestellen zijn bij Oxfam Fair Trade worden pas na enkele maanden uit de moederdatabank verwijderd (en dus uit jullie webshop), zodat we er zeker kunnen van zijn dat er geen lokale voorraden meer bestaan. Dit zal ook aangekondigd worden op het dashboard.</p>
 
@@ -56,14 +56,16 @@
 						if ( get_the_date('U') > strtotime('-3 months') ) $content .= ' new';
 						
 						// Check voorraadstatus van moederproduct, voeg klasse toe indien niet langer op stock
-						// VERTRAAGT DE BOEL NOGAL
-						$main_product_id = get_post_meta( get_the_ID(), '_woonet_network_is_child_product_id', true );
-						switch_to_blog(1);
+						// VERTRAAGT ENORM? KAN NU VIA DE LOKALE META KEY
+						// $main_product_id = get_post_meta( get_the_ID(), '_woonet_network_is_child_product_id', true );
+						// switch_to_blog(1);
 						// $main_product = wc_get_product( $main_product_id );
-						$bestelweb = get_post_meta( $main_product_id, '_in_bestelweb', true );
-						if ( $bestelweb === 'nee' ) $content .= ' old';
-						restore_current_blog();
-
+						// $bestelweb = get_post_meta( $main_product_id, '_in_bestelweb', true );
+						// restore_current_blog();
+						if ( $product->get_meta('_in_bestelweb') === 'nee' ) {
+							$content .= ' old';
+						}
+						
 						$content .= '">';
 							$content .= '<div class="cell" style="padding: 0.25em; width: 3%; text-align: center;"><a href="'.get_permalink().'" target="_blank">'.$product->get_image( 'wc_order_status_icon', null, false ).'</a></div>';
 							$content .= '<div class="cell '.$class.'" style="width: 40%; text-align: left;"><span class="title">'.$product->get_sku().': '.$product->get_title().' ('.$product->get_meta('_shopplus_code').')';
@@ -74,7 +76,7 @@
 							$content .= '<div class="cell"><select class="toggle" id="'.get_the_ID().'-stockstatus">';
 								$content .= '<option value="instock" '.selected( $product->is_in_stock(), true, false ).'>Op voorraad</option>';
 								$content .= '<option value="onbackorder" '.selected( $product->is_on_backorder(), true, false ).'>Tijdelijk uit voorraad</option>';
-								$content .= '<option value="outofstock" '.selected( $product->is_in_stock(), false, false ).'>Uitverkocht</option>';
+								$content .= '<option value="outofstock" '.selected( $product->is_in_stock(), false, false ).'>Niet in assortiment</option>';
 							$content .= '</select></div>';
 							$content .= '<div class="cell"><input class="toggle" type="checkbox" id="'.get_the_ID().'-featured" '.checked( $product->is_featured(), true, false ).'>';
 							$content .= ' <label for="'.get_the_ID().'-featured">In de kijker?</label></div>';
@@ -86,11 +88,11 @@
 				$content .= '</div>';
 				wp_reset_postdata();
 
-				echo '<p style="text-align: right; width: 100%;"><br>Deze pagina toont <b>'.$i.' producten</b>, waarvan er momenteel <b><span class="instock-cnt">'.$instock_cnt.'</span> voorradig</b> zijn en <b><span class="featured-cnt">'.$featured_cnt.'</span> in de kijker</b> staan op de homepage.</p>';
+				echo '<p style="text-align: right; width: 100%;"><br>Deze pagina toont <b>'.$i.' producten</b>, waarvan er momenteel <b><span class="instock-cnt">'.$instock_cnt.'</span> bestelbaar</b> zijn en <b><span class="featured-cnt">'.$featured_cnt.'</span> in de kijker</b> staan op de homepage.</p>';
 				
 				echo $content;
 				
-				echo '<p style="text-align: right; width: 100%;">Deze pagina toont <b>'.$i.' producten</b>, waarvan er momenteel <b><span class="instock-cnt">'.$instock_cnt.'</span> voorradig</b> zijn en <b><span class="featured-cnt">'.$featured_cnt.'</span> in de kijker</b> staan op de homepage.</p>';
+				echo '<p style="text-align: right; width: 100%;">Deze pagina toont <b>'.$i.' producten</b>, waarvan er momenteel <b><span class="instock-cnt">'.$instock_cnt.'</span> bestelbaar</b> zijn en <b><span class="featured-cnt">'.$featured_cnt.'</span> in de kijker</b> staan op de homepage.</p>';
 				
 			}
 
@@ -185,7 +187,7 @@
 						jQuery("#oxfam-products").find(".global-toggle").on( 'change', function() {
 							if ( jQuery(this).find(":selected").val() == 'instock' ) {
 								var to_change = jQuery("#oxfam-products").find(".border.color-red").length; 
-								var go = confirm("Ben je zeker dat je "+to_change+" producten in voorraad wil zetten?");
+								var go = confirm("Ben je zeker dat je "+to_change+" producten op voorraad wil zetten?");
 								if ( go == true ) {
 									jQuery(this).parent().parent().find(".output").html("Aan het verwerken ...");
 									jQuery("#oxfam-products").find(".border.color-red").parent().find("select.toggle").val('instock').each( function() {
@@ -202,7 +204,7 @@
 								}
 							} else if ( jQuery(this).find(":selected").val() == 'outofstock' ) {
 								var to_change = jQuery("#oxfam-products").find(".border.color-green").length; 
-								var go = confirm("Ben je zeker dat je "+to_change+" producten op uitverkocht wil zetten?");
+								var go = confirm("Ben je zeker dat je "+to_change+" producten uit assortiment wil zetten?");
 								if ( go == true ) {
 									jQuery(this).parent().parent().find(".output").html("Aan het verwerken ...");
 									jQuery("#oxfam-products").find(".border.color-green").parent().find("select.toggle").val('outofstock').each( function() {
@@ -229,7 +231,7 @@
 				<select class="global-toggle">';
 					<option value="" selected>(bulkwijziging)</option>
 					<option value="instock">Zet ALLE producten op voorraad</option>
-					<option value="outofstock">Zet ALLE producten op uitverkocht</option>
+					<option value="outofstock">Haal ALLE producten uit assortiment</option>
 				</select>
 			</div>
 			<div class="cell" style="width: 40%; text-align: left;">
