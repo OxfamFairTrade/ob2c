dfcde8780a2c7732374cc5083f9e32298528b192
Mac Pro <info@fullstackahead.be>
2020-10-08 05:54:54 +0200

    Update functions.php

--- uploads/wpallexport/functions.php
+++ uploads/wpallexport/functions.php
@@ -20,4 +20,91 @@
 		}
 		return implode( ', ', $may_contain );
 	}
+
+	function get_subcategory( $cats ) {
+		switch ( $cats ) {
+			case stristr( $cats, 'Koffie' ):
+				return 'Koffie & thee';
+				break;
+			case stristr( $cats, 'Ontbijt' ):
+				return 'Ontbijt';
+				break;
+			case stristr( $cats, 'Snacks' ):
+				return 'Snacks & drinks';
+				break;
+			case stristr( $cats, 'Wereldkeuken' ):
+				return 'Wereldkeuken';
+				break;
+			case stristr( $cats, 'Wijn' ):
+				return 'Wijn';
+				break;
+			default:
+				return $cats;
+				break;
+		}
+	}
+
+	function get_subcategory_fr( $cats ) {
+		switch ( $cats ) {
+			case stristr( $cats, 'Koffie' ):
+				return 'Café & thé';
+				break;
+			case stristr( $cats, 'Ontbijt' ):
+				return 'Petit déjeuner';
+				break;
+			case stristr( $cats, 'Snacks' ):
+				return 'Snacks & drinks';
+				break;
+			case stristr( $cats, 'Wereldkeuken' ):
+				return 'Cuisine du monde';
+				break;
+			case stristr( $cats, 'Wijn' ):
+				return 'Vin';
+				break;
+			default:
+				return $cats;
+				break;
+		}
+	}
+
+	function get_subcategory_en( $cats ) {
+		switch ( $cats ) {
+			case stristr( $cats, 'Koffie' ):
+				return 'Coffee & tea';
+				break;
+			case stristr( $cats, 'Ontbijt' ):
+				return 'Breakfast';
+				break;
+			case stristr( $cats, 'Snacks' ):
+				return 'Snacks & drinks';
+				break;
+			case stristr( $cats, 'Wereldkeuken' ):
+				return 'World kitchen';
+				break;
+			case stristr( $cats, 'Wijn' ):
+				return 'Wine';
+				break;
+			default:
+				return $cats;
+				break;
+		}
+	}
+
+	function get_bio_label( $bio ) {
+		if ( $bio == 'Ja' ) {
+			return ':biobol.psd';
+		} else {
+			return ':geenbio.psd';
+		}
+	}
+
+	function split_by_paragraph( $text ) {
+		$parts = explode( '</p><p>', $text );
+		$bits = explode( '<br>', $parts[0] );
+		return $bits[0];
+	}
+
+	function get_product_image( $sku ) {
+		return ':'.$sku.'.jpg';
+	}
 ?>
