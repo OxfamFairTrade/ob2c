b2f428a72b5f6dea3b8177f43b23eab8ee1f203d
Macbook Pro 15 <info@fullstackahead.be>
2020-10-14 08:59:55 +0200

    Update WC-templates

--- woocommerce/emails/email-order-details.php
+++ woocommerce/emails/email-order-details.php
@@ -2,63 +2,84 @@
 /**
  * Order details table shown in emails.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates/Emails
- * @version     3.0.0
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 3.7.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
 $text_align = is_rtl() ? 'right' : 'left';
 
 do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
 
-<?php if ( ! $sent_to_admin ) : ?>
-	<!-- GEWIJZIGD: Extra enter en geen hashtag voor bestelnummer -->	
-	<br/><h2><?php printf( 'Jouw bestelling (%s)', $order->get_order_number() ); ?></h2>
-<?php else : ?>
-	<!-- GEWIJZIGD: Extra enter, geen hashtag voor bestelnummer en alles aanklikbaar maken -->	
-	<br/><h2><a class="link" href="<?php echo esc_url( admin_url( 'post.php?post=' . $order->get_id() . '&action=edit' ) ); ?>"><b><?php printf( 'Bestelling %s', $order->get_order_number() ); ?> (<?php printf( '<time datetime="%s">%s</time>', $order->get_date_created()->format( 'c' ), wc_format_datetime( $order->get_date_created() ) ); ?>)</b></a></h2>
-<?php endif; ?>
+<br/>
+<h2>
+	<?php
+	if ( $sent_to_admin ) {
+		$before = '<a class="link" href="' . esc_url( $order->get_edit_order_url() ) . '">';
+		$after  = '</a>';
+	} else {
+		$before = '';
+		$after  = '';
+	}
+	// GEWIJZIGD: Geen hashtag voor bestelnummer
+	echo wp_kses_post( $before . sprintf( 'Bestelling %s' . $after . ' (<time datetime="%s">%s</time>)', $order->get_order_number(), $order->get_date_created()->format( 'c' ), wc_format_datetime( $order->get_date_created() ) ) );
+	?>
+</h2>
 
-<table class="td" cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" border="1">
-	<thead>
-		<tr>
-			<th class="td" scope="col" style="text-align:<?php echo $text_align; ?>;"><?php _e( 'Product', 'woocommerce' ); ?></th>
-			<th class="td" scope="col" style="text-align:<?php echo $text_align; ?>;"><?php _e( 'Quantity', 'woocommerce' ); ?></th>
-			<th class="td" scope="col" style="text-align:<?php echo $text_align; ?>;"><?php _e( 'Price', 'woocommerce' ); ?></th>
-		</tr>
-	</thead>
-	<tbody>
-		<!-- GEWIJZIGD: Fotootjes tonen aan klanten (met iets grotere thumbnail) en platte tekst uit -->
-		<?php echo wc_get_email_order_items( $order, array(
-			'show_sku'      => $sent_to_admin,
-			'show_image'    => !$sent_to_admin,
-			'image_size'    => array(64, 64),
-			'plain_text'    => false,
-			'sent_to_admin' => $sent_to_admin,
-		) ); ?>
-	</tbody>
-	<tfoot>
-		<?php
-			if ( $totals = $order->get_order_item_totals() ) {
+<div style="margin-bottom: 40px;">
+	<table class="td" cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" border="1">
+		<thead>
+			<tr>
+				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
+				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
+				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			// GEWIJZIGD: Fotootjes tonen aan klanten (met iets grotere thumbnail)
+			echo wc_get_email_order_items( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				$order,
+				array(
+					'show_sku'      => $sent_to_admin,
+					'show_image'    => ! $sent_to_admin,
+					'image_size'    => array( 64, 64 ),
+					'plain_text'    => $plain_text,
+					'sent_to_admin' => $sent_to_admin,
+				)
+			);
+			?>
+		</tbody>
+		<tfoot>
+			<?php
+			$item_totals = $order->get_order_item_totals();
+
+			if ( $item_totals ) {
 				$i = 0;
-				foreach ( $totals as $total ) {
+				foreach ( $item_totals as $total ) {
 					$i++;
-					?><tr>
-						<th class="td" scope="row" colspan="2" style="text-align:<?php echo $text_align; ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo $total['label']; ?></th>
-						<td class="td" style="text-align:<?php echo $text_align; ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo $total['value']; ?></td>
-					</tr><?php
+					?>
+					<tr>
+						<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['label'] ); ?></th>
+						<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>; <?php echo ( 1 === $i ) ? 'border-top-width: 4px;' : ''; ?>"><?php echo wp_kses_post( $total['value'] ); ?></td>
+					</tr>
+					<?php
 				}
 			}
-		?>
-	</tfoot>
-</table>
-
-<!-- GEWIJZIGD: Extra witruimte onder tabel -->
-<p>&nbsp;</p>
+			// GEWIJZIGD: Verwijderen want verhuisd naar klantinfo?
+			if ( $order->get_customer_note() ) {
+				?>
+				<tr>
+					<th class="td" scope="row" colspan="2" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Note:', 'woocommerce' ); ?></th>
+					<td class="td" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php echo wp_kses_post( nl2br( wptexturize( $order->get_customer_note() ) ) ); ?></td>
+				</tr>
+				<?php
+			}
+			?>
+		</tfoot>
+	</table>
+</div>
 
 <?php do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text, $email ); ?>
