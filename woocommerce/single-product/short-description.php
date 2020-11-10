cf4cf9e9e0daf1dfc116bd4fee35f98a999cf908
Mac Pro <info@fullstackahead.be>
2020-11-10 01:13:43 +0100

    Toon bij wijnen ook lange (sommeliers)beschrijving

--- woocommerce/single-product/short-description.php
+++ woocommerce/single-product/short-description.php
@@ -18,11 +18,16 @@ $short_description = apply_filters( 'woocommerce_short_description', $post->post
 
 if ( ! $short_description ) {
 	// Toon de lange beschrijving als alternatief
-	// TO DO: ook altijd tonen bij wijnen!
 	$short_description = get_the_content();
 }
 
 ?>
 <div class="woocommerce-product-details__short-description entry-content">
+	<?php
+		// Toon bij wijnen ook altijd de lange (sommeliers)beschrijving
+		if ( stripos( get_permalink(), '/wijn/' ) > 0 ) {
+			the_content();
+		}
+	?>
 	<?php echo $short_description; // WPCS: XSS ok. ?>
 </div>
