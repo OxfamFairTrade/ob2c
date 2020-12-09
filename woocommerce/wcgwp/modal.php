9132727fed1c31ac9feea5fa76a263f04c68f280
Mac Pro <info@fullstackahead.be>
2020-12-09 10:46:51 +0100

    Update modal.php

--- woocommerce/wcgwp/modal.php
+++ woocommerce/wcgwp/modal.php
@@ -51,12 +51,10 @@ defined( 'ABSPATH' ) || exit;
                 }
             </style>
 
-            <div class="modal-header">
-                <a href="#" class="giftwrap_cancel" data-dismiss="modal" aria-label="Close" title="Annuleer"></a>
-            </div>
-            
             <form class="giftwrapper_products modal_form wcgwp_form" method="post">
-                <div class="modal-body wcgwp_modal_body">
+                <div class="modal-header wcgwp_modal_body">
+                    <!-- Opgelet: klasse .modal-header hierboven is nodig om de knop te laten werken! -->
+                    <a href="#" class="giftwrap_cancel" data-dismiss="modal" aria-label="Close" title="Annuleer"></a>
                     <h3>Oxfam pakt (voor) je in!</h3>
                     <?php if ( ! apply_filters( 'wcgwp_hide_details', FALSE ) ) { ?>
                         <p class="giftwrap_details">
