6bed5855f79d2da5a51806a98cb971c583af3e25
Macbook Pro 13 <info@fullstackahead.be>
2020-12-21 10:03:42 +0100

    Update run-cronjobs-sitewide.php

--- plugins/run-cronjobs-sitewide.php
+++ plugins/run-cronjobs-sitewide.php
@@ -14,16 +14,16 @@ foreach ( $blogs as $blog ) {
 	$domain = $blog->domain;
 	$path = $blog->path;
 	
-	$command = '/usr/local/bin/php ' . dirname(__FILE__) . '/public_html' . ( $path ? $path : '/' ) . 'wp-cron.php doing_wp_cron';
-	// Gooit een 'Could not open input file' op maar werkt in de praktijk wel
-	$rc = shell_exec( $command );
+	// Gooit een 'Could not open input file' op en werkt in de praktijk enkel op het hoofdniveau
+	// $command = '/usr/local/bin/php ' . dirname(__FILE__) . '/public_html' . ( $path ? $path : '/' ) . 'wp-cron.php doing_wp_cron';
+	// $rc = shell_exec( $command );
 	
-	// Dit vreet I/O omdat het via de front-end loopt (en er ergens iets mis is?)
-	// $command = "https://" . $domain . ( $path ? $path : '/' ) . 'wp-cron.php?doing_wp_cron';
-	// $ch = curl_init( $command );
-	// $rc = curl_setopt( $ch, CURLOPT_RETURNTRANSFER, false );
-	// $rc = curl_exec( $ch );
-	// curl_close( $ch );
+	// Dit vreet I/O omdat het via de front-end loopt (is er ergens iets mis?)
+	$command = "https://" . $domain . ( $path ? $path : '/' ) . 'wp-cron.php?doing_wp_cron';
+	$ch = curl_init( $command );
+	$rc = curl_setopt( $ch, CURLOPT_RETURNTRANSFER, false );
+	$rc = curl_exec( $ch );
+	curl_close( $ch );
 
 	print_r( "✔ " . $command . PHP_EOL );
 }
