4ae816ec1a543229482405f4435b6bf405f734d8
Macbook Pro 13 <info@fullstackahead.be>
2020-12-24 11:13:35 +0100

    Laad MO-files uit transients (+ object cache)
    oplossing voor hoge I/O usage

--- plugins/run-cronjobs-sitewide.php
+++ plugins/run-cronjobs-sitewide.php
@@ -1,31 +1,30 @@
 <?php
 
-// Laad de WordPress-omgeving (relatief pad geldig vanuit domeinmap)
-require_once dirname(__FILE__).'/public_html/wp-load.php';
+// Laad de WordPress-omgeving (relatief pad geldig vanuit de domeinmap)
+require_once dirname(__FILE__) . '/public_html/wp-load.php';
 
-$args = array( 'public' => 1 );
-$blogs = get_sites( $args );
+$sites = get_sites( array( 'public' => 1, 'site__not_in' => get_site_option('oxfam_blocked_sites') ) );
 
 global $wp_version;
 $agent = 'WordPress/' . $wp_version . '; ' . home_url();
+$start = microtime(true);
 
-// Run cron on each blog
-foreach ( $blogs as $blog ) {
-	$domain = $blog->domain;
-	$path = $blog->path;
-	
+foreach ( $sites as $site ) {
 	// Gooit een 'Could not open input file' op en werkt in de praktijk enkel op het hoofdniveau
+	// $path = $site->path;
 	// $command = '/usr/local/bin/php ' . dirname(__FILE__) . '/public_html' . ( $path ? $path : '/' ) . 'wp-cron.php doing_wp_cron';
 	// $rc = shell_exec( $command );
 	
-	// Dit vreet I/O omdat het via de front-end loopt (is er ergens iets mis?)
-	$command = "https://" . $domain . ( $path ? $path : '/' ) . 'wp-cron.php?doing_wp_cron';
+	switch_to_blog( $site->blog_id );
+
+	$command = home_url( 'wp-cron.php?doing_wp_cron='.time() );
 	$ch = curl_init( $command );
 	$rc = curl_setopt( $ch, CURLOPT_RETURNTRANSFER, false );
 	$rc = curl_exec( $ch );
 	curl_close( $ch );
-
-	print_r( "✔ " . $command . PHP_EOL );
+	
+	echo number_format( microtime(true)-$start, 2, ',', '.' ) . ' s - ' . $command . '<br/>';
+	restore_current_blog();
 }
 
 ?>
--- plugins/run-cronjobs-sitewide.sh
+++
@@ -1,2 +0,0 @@
-cd /home/deb109351/domains/shop.oxfamwereldwinkels.be/public_html;
-/home/deb109351/bin/wp site list --field=url | xargs -i -n1 /home/deb109351/bin/wp cron event run --due-now --url="{}"
