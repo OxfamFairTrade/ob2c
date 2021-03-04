892358072755153074e320c148676594e5f5533e
Macbook Pro 13 <info@fullstackahead.be>
2021-03-04 02:52:30 +0100

    Update mailtemplates
    geven problemen met wachtwoord resets?

--- woocommerce/emails/customer-reset-password.php
+++ woocommerce/emails/customer-reset-password.php
@@ -2,21 +2,16 @@
 /**
  * Customer Reset Password email
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates/Emails
- * @version     2.0.0
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates\Emails
+ * @version 4.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+defined( 'ABSPATH' ) || exit;
 
 $customer = get_user_by( 'login', $user_login );
 
-?>
-
-<?php do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
 <p>
 	<?php
@@ -31,18 +26,29 @@ $customer = get_user_by( 'login', $user_login );
 
 <p>
 	<?php
-		printf( __( 'Iemand heeft verzocht om het wachtwoord van je account &laquo; %1$s &raquo; bij de webshop van %2$s opnieuw in te stellen.', 'oxfam-webshop' ), $user_login, get_webshop_name() );
+		printf( esc_html__( 'Iemand heeft verzocht om het wachtwoord van je account &laquo; %1$s &raquo; bij de webshop van %2$s opnieuw in te stellen.', 'oxfam-webshop' ), $user_login, get_webshop_name() );
 	?>
 </p>
 
-<p><?php _e( 'Was dit een vergissing? Negeer dan gewoon deze e-mail en er zal niets gebeuren.', 'oxfam-webshop' ); ?></p>
+<p><?php esc_html_e( 'Was dit een vergissing? Negeer dan gewoon deze e-mail en er zal niets gebeuren.', 'oxfam-webshop' ); ?></p>
 
 <p style="text-align: center;">
-	<a class="link" href="<?php echo esc_url( add_query_arg( array( 'key' => $reset_key, 'login' => rawurlencode( $user_login ) ), wc_get_endpoint_url( 'lost-password', '', wc_get_page_permalink( 'myaccount' ) ) ) ); ?>">
-		<b><?php _e( 'Speciale wachtwoordresetlink', 'oxfam-webshop' ); ?> &raquo;</b>
+	<a class="link" href="<?php echo esc_url( add_query_arg( array( 'key' => $reset_key, 'id' => $user_id ), wc_get_endpoint_url( 'lost-password', '', wc_get_page_permalink( 'myaccount' ) ) ) ); ?>">
+		<b><?php esc_html_e( 'Speciale wachtwoordresetlink', 'oxfam-webshop' ); ?> &raquo;</b>
 	</a>
 </p>
 
-<p><?php printf( __( 'Ondertekening van mails met accountinfo, inclusief regio van webshop (%s).', 'oxfam-webshop' ), get_webshop_name(true) ); ?></p>
+<?php
+	/**
+	 * Show user-defined additional content - this is set in each email's settings.
+	 */
+	if ( $additional_content ) {
+		echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+	}
+?>
 
-<?php do_action( 'woocommerce_email_footer', $email ); ?>
+<p><?php printf( esc_html__( 'Ondertekening van mails met accountinfo, inclusief regio van webshop (%s).', 'oxfam-webshop' ), get_webshop_name(true) ); ?></p>
+
+<?php
+
+do_action( 'woocommerce_email_footer', $email );
