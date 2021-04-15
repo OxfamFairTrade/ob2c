e791884d7f0094890cb0920862a3b3354ad3be48
Mac Pro <info@fullstackahead.be>
2021-04-15 13:04:52 +0200

    Update cart-totals.php

--- woocommerce/cart/cart-totals.php
+++ woocommerce/cart/cart-totals.php
@@ -27,7 +27,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 		</tr>
 
 		<div class="coupon digital-voucher">
-			<input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'ABC123' ); ?>" /> <input type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
+			<input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'ABC123' ); ?>" />
+			<button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
 			<?php do_action( 'woocommerce_cart_coupon' ); ?>
 		</div>
 
