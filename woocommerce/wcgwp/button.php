fea356eb6dddd968d42a685c3e7d6aa5983782b3
Mac Pro <info@fullstackahead.be>
2021-04-15 19:29:35 +0200

    Tweaks

--- woocommerce/wcgwp/button.php
+++ woocommerce/wcgwp/button.php
@@ -8,5 +8,5 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <div class="giftwrap_header_wrapper">
-    <p class="giftwrap_header"><a data-toggle="modal" data-target=".giftwrapper_products_modal<?php echo $label; ?>" class="wcgwp-modal-toggle wcgwp-modal-toggle<?php echo $label; ?> btn" data-label="<?php echo $label; ?>"><?php echo wp_kses_post( apply_filters( 'wcgwp_add_wrap_prompt', __( 'Add gift wrap?', 'woocommerce-gift-wrapper' ) ) ); ?></a></p>
+    <a data-toggle="modal" data-target=".giftwrapper_products_modal<?php echo $label; ?>" class="wcgwp-modal-toggle wcgwp-modal-toggle<?php echo $label; ?> btn" data-label="<?php echo $label; ?>"><?php echo wp_kses_post( apply_filters( 'wcgwp_add_wrap_prompt', __( 'Add gift wrap?', 'woocommerce-gift-wrapper' ) ) ); ?></a>
 </div>
