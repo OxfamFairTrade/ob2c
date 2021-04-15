5532b0507c513a74b78eadc985879b3e0f80caca
Mac Pro <info@fullstackahead.be>
2021-04-15 18:40:47 +0200

    Tweaks

--- woocommerce/cart/cart-totals.php
+++ woocommerce/cart/cart-totals.php
@@ -27,12 +27,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 		</tr>
 
 		<tr class="coupon digital-voucher">
-			<th>
-				<input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'ABC123' ); ?>" />
-			</th>
-			<td>
-				<button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
-			</td>
+			<th><input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Digitale cadeaubon? Vul hier je code in!', 'oxfam-webshop' ); ?>" /></th>
+			<td><button type="submit" class="button" name="apply_coupon"><?php esc_attr_e( 'Pas toe', 'oxfam-webshop' ); ?></button></td>
 			<?php do_action( 'woocommerce_cart_coupon' ); ?>
 		</tr>
 
