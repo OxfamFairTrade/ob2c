ba425c3404e623db257075de0d2c13f1d82a11bb
Mac Pro <info@fullstackahead.be>
2021-04-30 18:07:52 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1461,6 +1461,7 @@
 
 					(function(d, s, id) {
 						var js, fjs = d.getElementsByTagName(s)[0];
+						if (d.getElementById(id)) return;
 						js = d.createElement(s); js.id = id;
 						js.src = 'https://connect.facebook.net/nl_NL/sdk/xfbml.customerchat.js';
 						fjs.parentNode.insertBefore(js, fjs);
