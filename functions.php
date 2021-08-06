4df3e9faca6d9911d731caf31672b5949fe37813
Mac Pro <info@fullstackahead.be>
2021-08-06 19:02:06 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -7417,8 +7417,8 @@
 				// Het is momenteel niet werkbaar om de volledige productcatalogus van Magasins du Monde (+/- 2.500 voorradige producten) in het webshopnetwerk te pompen: dit stelt hogere eisen aan de productdata, de zoekfunctie, het voorraadbeheer, onze server, ... Bovendien is het voor de consument weinig zinvol om alle non-food te presenteren in onze nationale catalogus, gezien de beperkte lokale beschikbaarheid van de oudere craftsproducten.
 				//  De resterende aprilmagazineproducten en de nieuwe Doppers volgen morgen.
 				// echo '<div class="notice notice-success">';
-				// 	echo '<p>Drie nieuwe voedingsproducten (ter vervanging van uitdovende producten) werden toegevoegd aan de database:</p><ul style="margin-left: 2em; column-count: 2;">';
-				// 		$skus = array( 20699, 23696, 26017 );
+				// 	echo '<p>Twee nieuwe voedingsproducten en alle producten uit het augustusmagazine werden toegevoegd aan de database:</p><ul style="margin-left: 2em; column-count: 2;">';
+				// 		$skus = array( 25317, 27055 );
 				// 		foreach ( $skus as $sku ) {
 				// 			$product_id = wc_get_product_id_by_sku( $sku );
 				// 			if ( $product_id ) {
@@ -7432,9 +7432,9 @@
 				// 	}
 				// 	echo 'Pas wanneer een beheerder ze in voorraad plaatst, worden deze producten bestelbaar voor klanten.</p>';
 				// echo '</div>';
-				echo '<div class="notice notice-success">';
-					echo '<p>De <a href="https://copain.oww.be/k/n118/news/view/20167/12894/promo-s-online-winkel-juli-augustus-2021-update.html" target="_blank">promo\'s voor juli-augustus</a> zijn geactiveerd in alle webshops. De gratis clips JUSTE Tripel bij aankoop van minstens 50 euro (incl. kortingen, excl. verzendkosten) die tijdens de maand mei automatisch toegekend werden op online bestellingen zullen eind <del>juni</del> juli gecrediteerd worden, samen met de rollijst, en dienen <u>niet</u> doorgegeven te worden via Copain.</p>';
-				echo '</div>';
+				// echo '<div class="notice notice-success">';
+				// 	echo '<p>De <a href="https://copain.oww.be/k/n118/news/view/20167/12894/promo-s-online-winkel-juli-augustus-2021-update.html" target="_blank">promo\'s voor juli-augustus</a> zijn geactiveerd in alle webshops. De gratis clips JUSTE Tripel bij aankoop van minstens 50 euro (incl. kortingen, excl. verzendkosten) die tijdens de maand mei automatisch toegekend werden op online bestellingen zullen eind <del>juni</del> juli gecrediteerd worden, samen met de rollijst, en dienen <u>niet</u> doorgegeven te worden via Copain.</p>';
+				// echo '</div>';
 				// echo '<div class="notice notice-info">';
 				// 	echo '<p>Er werden twee geschenkverpakkingen toegevoegd: een geschenkmand (servicekost: 3,95 euro, enkel afhaling) en een geschenkdoos (servicekost: 2,50 euro, ook thuislevering). Door minstens één product op voorraad te zetten activeer je de module. Onder het winkelmandje verschijnt dan een opvallende knop om een geschenkverpakking toe te voegen. <a href="https://github.com/OxfamFairTrade/ob2c/wiki/9.-Lokaal-assortiment#geschenkverpakkingen" target="_blank">Raadpleeg de handleiding voor info over de werking en hoe je zelf geschenkverpakkingen kunt aanmaken met andere prijzen/voorwaarden.</a> Opmerking: indien je thuislevering van breekbare goederen inschakelde onder \'<a href="admin.php?page=oxfam-options">Winkelgegevens</a>\' kan de geschenkmand ook thuisgeleverd worden.</p>';
 				// echo '</div>';
