cea7416a919bb631f1a00e5b28ce599a74137caf
Frederik Neirynck <info@fullstackahead.be>
2022-02-02 21:00:06 +0100

    Update short-description.php

--- woocommerce/single-product/short-description.php
+++ woocommerce/single-product/short-description.php
@@ -13,29 +13,37 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $post;
-
 $short_description = apply_filters( 'woocommerce_short_description', $post->post_excerpt );
-
-if ( ! $short_description ) {
-	// Toon de lange beschrijving als alternatief
-	$short_description = get_the_content();
-}
+$long_description = get_the_content();
 
 ?>
 <div class="woocommerce-product-details__short-description entry-content">
 	<?php
-		// Probleem: als het product een uitgelichte partner heeft (+ het géén wijn is) verschijnt de lange producttekst nergens meer ...
-		// Logica om $featured_partner te bepalen verhuizen van product-origin.php naar content-single-product.php, zodat die info hier ook al beschikbaar is?
-		// global $featured_partner;
-		// if ( $featured_partner ) {
-		// 	echo the_content();
-		// } else {
+		if ( is_national_product( $post->ID ) ) {		
+			if ( stripos( get_permalink(), '/wijn/' ) > 0 ) {
+				// Wijn: toon eerst korte 'lekker bij'-omschrijving, en vervolgens lange sommeliersbeschrijving
+				echo $short_description;
+				echo $long_description;
+			} else {
+				if ( ! $short_description ) {
+					// Toon enkel lange beschrijving
+					echo $long_description;
+				} else {
+					// Als het product géén uitgelichte partner heeft, zou hier enkel de korte beschrijving moeten verschijnen ...
+					echo $short_description;
+					echo $long_description;
+					
+					// Logica om $featured_partner te bepalen verhuizen van product-origin.php naar content-single-product.php, zodat die info hier ook al beschikbaar is?
+					// global $featured_partner;
+					// if ( $featured_partner ) {
+					// 	echo $long_description;
+					// } else {
+					// 	echo $short_description;
+					// }
+				}
+			}
+		} else {
 			echo $short_description;
-		// }
-	
-		// Toon bij wijnen ook altijd de lange (sommeliers)beschrijving bovenaan
-		if ( stripos( get_permalink(), '/wijn/' ) > 0 ) {
-			the_content();
 		}
 	?>
 </div>
