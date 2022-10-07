7293c794f851d367ca0ddefc8e42788fe0f2d03c
Frederik Neirynck <info@fullstackahead.be>
2022-10-07 19:44:27 +0200

    Nieuwe credit refs 08935 en 08936
    + toevoegen vervaldatum als parameter voor linken van vouchercode aan juiste credit ref

--- export-used-vouchers.php
+++ export-used-vouchers.php
@@ -20,25 +20,25 @@
 		$end_date = date_i18n( 'Y-m-d', strtotime('last day of previous month') );
 		echo '<p><b>Startdatum</b>: '.$start_date.'<br/><b>Einddatum</b>: '.$end_date.'</p>';
 		$voucher_ids = array();
-
+		
 		// Haal standaard data van voorbije maand op
 		$distribution = get_credit_report_used_vouchers( $start_date, $end_date );
-
+		
 		// Haal specifieke data van bepaalde periode op
 		// $distribution = get_credit_report_used_vouchers( '2021-10-25', '2021-11-30', array( '08918' => array( 'issuer' => false, 'value' => 'faircaps21' ) ) );
-
+		
 		if ( count( $distribution ) > 0 ) {
 			// Toon resultaat op scherm
 			// var_dump_pre( $distribution );
-
+			
 			// Schrijf resultaat weg naar Excel
 			export_to_excel( $distribution );
 		} else {
 			echo '<p><b><span style="color: red">Er valt deze maand niets (meer) te crediteren!</span></b></p>';
 		}
-
+		
 		output_latest_exports( $start_date, $end_date );
-
+		
 		add_action( 'admin_footer', 'close_voucher_export' );
 	?>
 
@@ -47,45 +47,49 @@
 	<?php
 		function get_credit_report_used_vouchers( $start_date = '2021-05-01', $end_date = '2021-05-31', $credit_refs = false ) {
 			$distribution = array();
-
+			
 			if ( $credit_refs === false ) {
 				$credit_refs = array(
-					'08899' => array( 'issuer' => 'Gezinsbond', 'value' => 50 ),
-					'08900' => array( 'issuer' => 'Gezinsbond', 'value' => 25 ),
-					'08917' => array( 'issuer' => 'Cera', 'value' => 30 ),
-					'08924' => array( 'issuer' => 'CM', 'value' => 10 ),
+					// Geldig t.e.m. 31/12/2022
+					'08899' => array( 'issuer' => 'Gezinsbond', 'value' => 50, 'expires' => '2023-01-01' ),
+					'08900' => array( 'issuer' => 'Gezinsbond', 'value' => 25, 'expires' => '2023-01-01' ),
+					'08917' => array( 'issuer' => 'Cera', 'value' => 30, 'expires' => '2023-01-01' ),
+					'08924' => array( 'issuer' => 'CM', 'value' => 10, 'expires' => '2023-01-01' ),
+					// Geldig t.e.m. 31/12/2023
+					'08935' => array( 'issuer' => 'Gezinsbond', 'value' => 50, 'expires' => '2024-01-01' ),
+					'08936' => array( 'issuer' => 'Gezinsbond', 'value' => 25, 'expires' => '2024-01-01' ),
 				);
 			}
-
+			
 			foreach ( $credit_refs as $credit_ref => $credit_params ) {
-				$rows = get_number_of_times_voucher_was_used( $credit_refs[ $credit_ref ]['issuer'], $credit_refs[ $credit_ref ]['value'], $start_date, $end_date );
+				$rows = get_number_of_times_voucher_was_used( $credit_refs[ $credit_ref ]['issuer'], $credit_refs[ $credit_ref ]['value'], $start_date, $end_date, $credit_refs[ $credit_ref ]['expires'] );
 				echo '<p><b>Totaalbedrag te crediteren onder referentie '.$credit_ref.': '.wc_price( array_sum( $rows ) * $credit_refs[ $credit_ref ]['value'] ).'</b></p>';
-
+				
 				if ( count( $rows ) > 0 ) {
 					$distribution[ $credit_ref ] = $rows;
 				}
 			}
-
+			
 			return $distribution;
 		}
-
-		function get_number_of_times_voucher_was_used( $issuer, $value, $start_date, $end_date ) {
+		
+		function get_number_of_times_voucher_was_used( $issuer, $value, $start_date, $end_date, $expires = false ) {
 			global $wpdb, $voucher_ids;
 			$repartition = array();
 			$warnings = array();
-
+			
 			if ( $issuer === false ) {
-
+				
 				// Licht andere logica voor reguliere kortingsbonnen die niet in universele tabel zitten
 				$sites = get_sites( array( 'site__not_in' => get_site_option('oxfam_blocked_sites'), 'public' => 1 ) );
-
+				
 				foreach ( $sites as $site ) {
 					switch_to_blog( $site->blog_id );
 					foreach ( get_number_of_times_coupon_was_used( $value, $start_date, $end_date, true ) as $order ) {
 						if ( $order->get_status() !== 'completed' ) {
 							$warnings[ $order->get_order_number() ] = 'Bestelling <a href="'.$order->get_edit_order_url().'" target="_blank">'.$order->get_order_number().'</a> is nog niet afgerond, toch opgenomen in export';
 						}
-
+						
 						if ( is_regional_webshop() ) {
 							$blog_path = $order->get_meta('claimed_by');
 						} else {
@@ -93,41 +97,41 @@
 							// Wis de bewoording 'regio' bij pseudo-regiowebshops, om verwarring bij het ingeven in Access te vermijden
 							$blog_path = str_replace( array( '/regio', '/' ), '', $current_blog->path );
 						}
-
+						
 						if ( strlen( $blog_path ) < 1 ) {
 							$warnings[ $order->get_order_number() ] = 'Bestelling <a href="'.$order->get_edit_order_url().'" target="_blank">'.$order->get_order_number().'</a> is niet toegekend aan een winkel, niet opgenomen in export';
 							continue;
 						}
-
+						
 						if ( ! array_key_exists( $blog_path, $repartition ) ) {
 							$repartition[ $blog_path ] = 1;
 						} else {
 							$repartition[ $blog_path ] += 1;
 						}
-
+						
 						// Door gebrek aan $voucher_ids werkt de 'Bevestig creditering'-knop niet voor deze orders, dus voeg eventueel meteen toe
 						// $order->add_order_note( 'Kortingsbon '.strtoupper( $value ).' werd op 1 december 2021 door het NS gecrediteerd aan OWW '.ucfirst( $blog_path ).'.', 0, false );
 						// write_log( "Crediteringsnota toegevoegd aan ".$order->get_order_number() );
 					}
 					restore_current_blog();
 				}
-
+				
 			} else {
-
+				
 				// Vereis een lege 'credited'-datum zodat we verhinderen dat vouchers twee keer in een opgeslagen export kunnen opduiken
-				$query = "SELECT * FROM {$wpdb->base_prefix}universal_coupons WHERE `issuer` = '%s' AND value = %d AND DATE(`used`) BETWEEN '%s' AND '%s' AND DATE(`credited`) < '2001-01-01'";
-				$rows = $wpdb->get_results( $wpdb->prepare( $query, $issuer, $value, $start_date, $end_date ) );
-
+				$query = "SELECT * FROM {$wpdb->base_prefix}universal_coupons WHERE `issuer` = '%s' AND value = %d AND DATE(`used`) BETWEEN '%s' AND '%s' AND DATE(`credited`) < '2001-01-01' AND `expires` = '%s'";
+				$rows = $wpdb->get_results( $wpdb->prepare( $query, $issuer, $value, $start_date, $end_date, $expires ) );
+				
 				foreach ( $rows as $key => $row ) {
 					switch_to_blog( $row->blog_id );
-
+					
 					$args = array(
 						'type' => 'shop_order',
 						'order_number' => $row->order,
 						'limit' => -1,
 					);
 					$orders = wc_get_orders( $args );
-
+					
 					if ( $row->order !== 'OFFLINE' and count( $orders ) === 0 ) {
 						$warnings[ $row->order ] = 'Bestelling '.$row->order.' niet gevonden, voucher '.$row->code.' niet opgenomen in export';
 					} elseif ( count( $orders ) > 1 ) {
@@ -136,8 +140,16 @@
 						$current_blog = get_blog_details();
 						if ( $row->order === 'OFFLINE' ) {
 							$voucher_ids[] = $row->id;
-							// Wis de bewoording 'regio' bij pseudo-regiowebshops, om verwarring bij het ingeven in Access te vermijden
-							$blog_path = str_replace( array( '/regio', '/' ), '', $current_blog->path );
+							if ( intval( $row->id ) > 0 ) {
+								// Wis de bewoording 'regio' bij pseudo-regiowebshops, om verwarring bij het ingeven in Access te vermijden
+								$blog_path = str_replace( array( '/regio', '/' ), '', $current_blog->path );
+							} else {
+								if ( in_array( $row->code, array( 'NLU9NTVRSLQZ', 'XC1GTSEGC1Q3' ) ) ) {
+									$blog_path = 'dendermonde';
+								} else {
+									$blog_path = 'UNKNOWN';
+								}
+							}
 						} else {
 							$order = reset( $orders );
 							if ( $order->get_status() !== 'completed' ) {
@@ -145,20 +157,20 @@
 								restore_current_blog();
 								continue;
 							}
-
+							
 							if ( is_regional_webshop() ) {
 								$blog_path = $order->get_meta('claimed_by');
 							} else {
 								// Wis de bewoording 'regio' bij pseudo-regiowebshops, om verwarring bij het ingeven in Access te vermijden
 								$blog_path = str_replace( array( '/regio', '/' ), '', $current_blog->path );
 							}
-
+							
 							if ( strlen( $blog_path ) < 1 ) {
 								$warnings[ $row->order ] = 'Bestelling <a href="'.$order->get_edit_order_url().'" target="_blank">'.$row->order.'</a> is niet toegekend aan een winkel, voucher '.$row->code.' niet opgenomen in export';
 								restore_current_blog();
 								continue;
 							}
-
+							
 							$voucher_ids[] = $row->id;
 							$refunds = $order->get_refunds();
 							if ( count( $refunds ) > 0 ) {
@@ -175,66 +187,66 @@
 								$warnings[ $row->order ] = $warning;
 							}
 						}
-
+						
 						if ( ! array_key_exists( $blog_path, $repartition ) ) {
 							$repartition[ $blog_path ] = 1;
 						} else {
 							$repartition[ $blog_path ] += 1;
 						}
 					}
-
+					
 					restore_current_blog();
 				}
-
+				
 			}
-
+			
 			// Sorteer alfabetisch op naam van de winkel
 			ksort( $repartition );
-
+			
 			if ( count( $warnings ) > 0 ) {
 				// Sorteer op ordernummer
 				ksort( $warnings );
 				echo '<p>Waarschuwingen:</p><ol><li>'.implode( '</li><li>', $warnings ).'</li></ol>';
 			}
-
+			
 			return $repartition;
 		}
-
+		
 		function export_to_excel( $distribution ) {
-				$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-				$spreadsheet = $reader->load( get_stylesheet_directory().'/voucher-export.xlsx' );
-				$empty_sheet = clone $spreadsheet->getSheet(0);
-				foreach ( $distribution as $credit_ref => $number_of_credits_per_shop ) {
-					$i = 2;
-
-					try {
-						$spreadsheet->setActiveSheetIndexByName( $credit_ref );
-					} catch (Exception $e) {
-						// Maak het werkblad aan indien de referentie nog niet bestaat in de template, anders fatale error!
-						write_log("Excel-werkblad bestaat nog niet, probeer het aan te maken ...");
-						$empty_sheet->setTitle( $credit_ref );
-						$spreadsheet->addSheet( $empty_sheet );
-						$spreadsheet->setActiveSheetIndexByName( $credit_ref );
-					}
-
-					foreach ( $number_of_credits_per_shop as $shop => $numbers_of_credits ) {
-						$spreadsheet->getActiveSheet()->setCellValue( 'A'.$i, $shop )->setCellValue( 'B'.$i, $numbers_of_credits );
-						$i++;
-					}
+			$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+			$spreadsheet = $reader->load( get_stylesheet_directory().'/voucher-export.xlsx' );
+			$empty_sheet = clone $spreadsheet->getSheet(0);
+			foreach ( $distribution as $credit_ref => $number_of_credits_per_shop ) {
+				$i = 2;
+				
+				try {
+					$spreadsheet->setActiveSheetIndexByName( $credit_ref );
+				} catch (Exception $e) {
+					// Maak het werkblad aan indien de referentie nog niet bestaat in de template, anders fatale error!
+					write_log("Excel-werkblad bestaat nog niet, probeer het aan te maken ...");
+					$empty_sheet->setTitle( $credit_ref );
+					$spreadsheet->addSheet( $empty_sheet );
+					$spreadsheet->setActiveSheetIndexByName( $credit_ref );
 				}
-				$writer = new Xlsx( $spreadsheet );
-				$writer->save( WP_CONTENT_DIR . '/exports/latest.xlsx' );
+				
+				foreach ( $number_of_credits_per_shop as $shop => $numbers_of_credits ) {
+					$spreadsheet->getActiveSheet()->setCellValue( 'A'.$i, $shop )->setCellValue( 'B'.$i, $numbers_of_credits );
+					$i++;
+				}
 			}
-
+			$writer = new Xlsx( $spreadsheet );
+			$writer->save( WP_CONTENT_DIR . '/exports/latest.xlsx' );
+		}
+			
 		function output_latest_exports( $start_date, $end_date ) {
 			global $voucher_ids;
 			$files = get_latest_exports('xlsx');
-
+			
 			foreach ( $files as $file ) {
 				$id = '';
 				$title = ucwords( str_replace( '-', ' ', $file['name'] ) );
 				$extras = '';
-
+				
 				if ( $file['name'] === 'latest.xlsx' ) {
 					$id = 'latest';
 					$title = 'Download openstaande export';
@@ -243,16 +255,16 @@
 						$extras = ' <button id="'.$id.'" data-voucher-ids="'.implode( ',', $voucher_ids ).'" data-start-date="'.str_replace( '-', '', $start_date ).'" data-end-date="'.str_replace( '-', '', $end_date ).'" class="button confirm-export" disabled>Bevestig creditering</button>';
 					}
 				}
-
+				
 				// Om downloadlink te leggen naar niet-publieke map hebben we een download manager nodig ...
 				echo '<br/><a href="'.content_url( '/exports/'.$file['name'] ).'" download><button id="'.$id.'" class="button download-excel">'.$title.'</button></a>'.$extras.'<br/>';
 			}
 		}
-
+		
 		function get_latest_exports( $extension = false, $max = false ) {
 			$exports = array();
 			$local_path = WP_CONTENT_DIR.'/exports/';
-
+			
 			if ( $handle = opendir( $local_path ) ) {
 				// Loop door alle files in de map
 				while ( false !== ( $file = readdir( $handle ) ) ) {
@@ -270,10 +282,10 @@
 				}
 				closedir( $handle );
 			}
-
+			
 			// Orden in dalende chronologisch volgorde
 			usort( $exports, 'sort_by_time' );
-
+			
 			if ( $max ) {
 				// Geef enkel de eerste X weer
 				return array_slice( $exports, 0, $max );
@@ -281,13 +293,13 @@
 				return $exports;
 			}
 		}
-
+		
 		function close_voucher_export() {
 			?>
 			<script type="text/javascript">
 				jQuery(document).ready(function() {
 					jQuery(".confirm-export").prop( "disabled", false );
-
+					
 					jQuery(".confirm-export").on( "click", function() {
 						var button = jQuery(this);
 						var go = confirm("Ben je zeker dat deze lijst wil afsluiten? Alle vouchers uit de Excel worden als uitbetaald gemarkeerd in de database en zullen niet meer opduiken in volgende exports! Bij de bestellingen in kwestie wordt een nota toegevoegd dat de cadeaubon op <?php echo date_i18n( 'd/m/Y', strtotime( '+1 weekday', strtotime('last day of this month') ) ); ?> gecrediteerd zal worden.");
@@ -298,10 +310,10 @@
 							closeCurrentList(button);
 						}
 					});
-
+					
 					var tries = 0;
 					var max = 5;
-
+					
 					function closeCurrentList(button) {
 						jQuery.ajax({
 							type: 'POST',
@@ -342,11 +354,11 @@
 			</script>
 			<?php
 		}
-
+		
 		function ends_with( $haystack, $needle ) {
 			return $needle === '' or ( ( $temp = strlen( $haystack ) - strlen( $needle ) ) >= 0 and strpos( $haystack, $needle, $temp ) !== false );
 		}
-
+		
 		function sort_by_time( $a, $b ) {
 			return $b['timestamp'] - $a['timestamp'];
 		}
