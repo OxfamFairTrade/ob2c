fc69d794bcc568e93b0dbd7bdcf331dfa1fe16e5
Frederik Neirynck <info@fullstackahead.be>
2022-12-20 11:59:32 +0100

    Typo

--- functions/mailchimp/create-shopplus-products.php
+++ functions/mailchimp/create-shopplus-products.php
@@ -4,7 +4,7 @@
 	// Relatief pad enkel geldig vanuit subfolder in subfolder van themamap!
 	require_once '../../../../../wp-load.php';
 	
-	require_once WP_PLUGIN_DIR.'/mailchimp-api-wrapper.php');
+	require_once WP_PLUGIN_DIR.'/mailchimp-api-wrapper.php';
 	use \DrewM\MailChimp\MailChimp;
 	
 	if ( ! current_user_can('update_core') ) {
--- functions/mailchimp/subscribe.php
+++ functions/mailchimp/subscribe.php
@@ -6,7 +6,7 @@
 	// Relatief pad enkel geldig vanuit subfolder in subfolder van themamap!
 	require_once '../../../../../wp-load.php';
 	
-	require_once WP_PLUGIN_DIR.'/mailchimp-api-wrapper.php');
+	require_once WP_PLUGIN_DIR.'/mailchimp-api-wrapper.php';
 	use \DrewM\MailChimp\MailChimp;
 	
 	$response = array(
