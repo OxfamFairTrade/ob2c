487401f825a53fa5fe31dd86e2db4e4a556cd224
Frederik Neirynck <info@fullstackahead.be>
2023-02-07 03:39:33 +0100

    Update featured-partner.php

--- template-parts/woocommerce/single-product/featured-partner.php
+++ template-parts/woocommerce/single-product/featured-partner.php
@@ -7,15 +7,19 @@
 	<div class="col-row">
 		<div class="col-md-7">
 			<?php if ( array_key_exists( 'image', $featured_partner ) and $featured_partner['image'] !== '' ) : ?>
-				<img src="<?php echo esc_url( $featured_partner['image'] ); ?>">
+				<img src="<?= esc_url( $featured_partner['image'] ); ?>">
 			<?php elseif ( $featured_partner['quote']['image'] !== '' ) : ?>
-				<img src="<?php echo esc_url( $featured_partner['quote']['image'] ); ?>">
+				<img src="<?= esc_url( $featured_partner['quote']['image'] ); ?>">
 			<?php endif; ?>
 			
 			<?php if ( array_key_exists( 'acf', $featured_partner ) and $featured_partner['acf']['partner_bullet_points'] !== '' ) : ?>
-				<ul><li><?php implode( '</li><li>', $featured_partner['acf']['partner_bullet_points'] ); ?></li></ul>
+				<ul>
+					<?php foreach ( $featured_partner['acf']['partner_bullet_points'] as $bullet ) : ?>
+						<li><?= $bullet['partner_bullet_point']; ?></li>
+					<?php endforeach; ?>
+				</ul>
 			<?php else : ?>
-				<?php echo $featured_partner['bullet_points']; ?>
+				<?= $featured_partner['bullet_points']; ?>
 			<?php endif; ?>
 			
 			<p><a href="<?php echo esc_url( $featured_partner['link'] ); ?>">Maak kennis met <?php echo $featured_partner['name']; ?></a></p>
@@ -23,9 +27,9 @@
 		<div class="col-md-5">
 			<?php if ( ! empty( $featured_partner['quote']['content'] ) ) : ?>
 				<blockquote>
-					&#8220;<?php echo $featured_partner['quote']['content']; ?>&#8221;
+					&#8220;<?= $featured_partner['quote']['content']; ?>&#8221;
 					<?php if ( ! empty( $featured_partner['quote']['by'] ) ) : ?>
-						<footer><?php echo $featured_partner['quote']['by']; ?></footer>
+						<footer><?= $featured_partner['quote']['by']; ?></footer>
 					<?php endif; ?>
 				</blockquote>
 			<?php endif; ?>
