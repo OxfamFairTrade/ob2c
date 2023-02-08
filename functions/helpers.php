49a687a3b75c22389790bb579120e6b1260959f1
Frederik Neirynck <info@fullstackahead.be>
2023-02-08 23:53:05 +0100

    Update helpers.php

--- functions/helpers.php
+++ functions/helpers.php
@@ -2,6 +2,67 @@
 	
 	if ( ! defined('ABSPATH') ) exit;
 	
+	add_action( 'clean_old_product_terms', 'clean_old_product_terms_callback', 10, 1 );
+	
+	function clean_old_product_terms_callback( $args ) {
+		switch_to_blog( $args['site_id'] );
+		
+		$cnt = 0;
+		$start = microtime(true);
+		$to_delete = array(
+			'pa_bio',
+			'pa_choavl',
+			'pa_ean',
+			'pa_eenheid',
+			'pa_ener',
+			'pa_eprijs',
+			'pa_fairtrade',
+			'pa_famscis',
+			'pa_fapucis',
+			'pa_fasat',
+			'pa_fat',
+			'pa_fibtg',
+			'pa_ompak',
+			'pa_polyl',
+			'pa_pro',
+			'pa_salteq',
+			'pa_shopplus',
+			'pa_starch',
+			'pa_sugar',
+			'product_allergen',
+			'product_grape',
+			'product_recipe',
+			'product_taste',
+		);
+		
+		foreach ( $to_delete as $taxonomy ) {
+			// Anders vinden we niks, ook al zwerven ze nog rond in de database!
+			register_taxonomy( $taxonomy, 'product' );
+			
+			$terms = get_terms( array( 'taxonomy' => $taxonomy, 'hide_empty' => false ) );
+			foreach ( $terms as $term ) {
+				$name = $term->name;
+				if ( wp_delete_term( $term->term_id, $taxonomy ) ) {
+					$cnt++;
+				}
+			}
+		}
+		write_log( get_bloginfo('name').": deleted ".$cnt." terms in ".number_format( microtime(true)-$start, 2, ',', '.' )." seconds" );
+		
+		restore_current_blog();
+		
+		if ( $args['site_id'] < 86 ) {
+			$args['site_id'] += 1;
+			if ( as_schedule_single_action( strtotime(), 'clean_old_product_terms', $args, 'Cleanup' ) > 0 ) {
+				write_log("Cleanup for site-ID ".$args['site_id']." scheduled");
+			}
+		} else {
+			write_log("Cleanup terminated");
+		}
+	}
+	
+	
+	
 	#############
 	# POSTCODES #
 	#############
