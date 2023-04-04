bafcd0d66bfd28d2ecd18a9fe99389f6d86ad7c1
Frederik Neirynck <info@fullstackahead.be>
2023-04-05 01:41:29 +0200

    Update do-voucher-lookup.php

--- functions/vouchers/do-voucher-lookup.php
+++ functions/vouchers/do-voucher-lookup.php
@@ -5,7 +5,7 @@
 <div class="wrap">
 	<h1>Opzoeken van digicheque</h1>
 	
-	<p>Met behulp van dit formulier kun je de status van een digicheque opzoeken, zonder toegang te moeten hebben tot de database.<br/>In geval van nood kun je de digicheque ook handmatig ongeldig maken (bv. omdat een winkel de code toch aanvaardde in de winkel). Opgelet: in dat geval dien je zelf te onthouden aan wie de code gecrediteerd moet worden! De code zal onder de winkelnaam 'UNKNOWN' toegevoegd worden aan de maandelijkse export.</p>
+	<p>Met behulp van dit formulier kun je de status van een digicheque opzoeken, zonder rechtstreeks in de database te moeten werken.<br/>In geval van nood kun je de digicheque ook handmatig ongeldig maken (bv. omdat een winkel de code toch aanvaardde in de winkel).<br/>Opgelet: in dat geval dien je zelf te onthouden aan wie de code gecrediteerd moet worden! De code zal onder de winkelnaam 'UNKNOWN' opduiken in de maandelijkse export.</p>
 	
 	<input type="text" id="voucher-code" name="voucher-code" class="" placeholder="DEMODEMODEMO">
 	<button type="submit" id="voucher-code-lookup" name="voucher-code-lookup">Zoek</button>
@@ -17,8 +17,8 @@
 <script type="text/javascript">
 	jQuery(document).ready( function() {
 		jQuery('#voucher-code-lookup').on( 'click', function() {
-			jQuery('#result').html( jQuery('#voucher-code-lookup').val() );
-			jQuery('#result').show();
+			jQuery('#result').html( jQuery('#voucher-code').val() );
+			jQuery('#result').slideDown();
 		});
 	});
 </script>
