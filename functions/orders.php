d6787fc45c677c71e122043df099bd5e25c764f6
Frederik Neirynck <info@fullstackahead.be>
2023-04-19 18:42:52 +0200

    Verberg default WooCommerce-widgets via CSS

--- functions/orders.php
+++ functions/orders.php
@@ -6,58 +6,60 @@
 	add_action( 'woocommerce_after_dashboard_status_widget', 'add_custom_dashboard_widgets', 10, 1 );
 	
 	function add_custom_dashboard_widgets( $reports ) {
-		$on_hold_count = 0;
-		$processing_count = 0;
+		$orders_to_claim = 0;
+		$orders_to_pick = 0;
+		$orders_to_supply = 0;
 		
 		foreach ( wc_get_order_types('order-count') as $type ) {
 			$counts = (array) wp_count_posts( $type );
-			$processing_count += isset( $counts['wc-processing'] ) ? $counts['wc-processing'] : 0;
 			if ( is_regional_webshop() ) {
+				$orders_to_claim += isset( $counts['wc-processing'] ) ? $counts['wc-processing'] : 0;
 				// Vervang 'on-hold' door interessantere status bij regiowerkingen
-				$on_hold_count += isset( $counts['wc-claimed'] ) ? $counts['wc-claimed'] : 0;
+				$orders_to_pick += isset( $counts['wc-claimed'] ) ? $counts['wc-claimed'] : 0;
 			} else {
-				$on_hold_count += isset( $counts['wc-on-hold'] ) ? $counts['wc-on-hold'] : 0;
+				$orders_to_pick += isset( $counts['wc-processing'] ) ? $counts['wc-processing'] : 0;
+				$orders_to_supply += isset( $counts['wc-on-hold'] ) ? $counts['wc-on-hold'] : 0;
 			}
 		}
 		?>
 		<?php if ( is_regional_webshop() ) : ?>
-			<li class="processing-orders">
+			<li class="orders-to-claim">
 				<a href="<?php echo esc_url( admin_url( 'edit.php?post_status=wc-processing&post_type=shop_order' ) ); ?>">
 				<?php
 					printf(
-						_n( '<strong>%s bestelling</strong> wacht op bevestiging', '<strong>%s bestellingen</strong> wachten op bevestiging', $processing_count, 'woocommerce' ),
-						$processing_count
+						_n( '<strong>%s bestelling</strong> wacht op bevestiging', '<strong>%s bestellingen</strong> wachten op bevestiging', $orders_to_claim, 'woocommerce' ),
+						$orders_to_claim
 					);
 				?>
 				</a>
 			</li>
-			<li class="claimed-orders">
+			<li class="orders-to-pick">
 				<a href="<?php echo esc_url( admin_url( 'edit.php?post_status=wc-claimed&post_type=shop_order' ) ); ?>">
 				<?php
 					printf(
-						_n( '<strong>%s order</strong> awaiting processing', '<strong>%s orders</strong> awaiting processing', $on_hold_count, 'woocommerce' ),
-						$on_hold_count
+						_n( '<strong>%s order</strong> awaiting processing', '<strong>%s orders</strong> awaiting processing', $orders_to_pick, 'woocommerce' ),
+						$orders_to_pick
 					);
 				?>
 				</a>
 			</li>
 		<?php else : ?>
-			<li class="processing-orders">
+			<li class="orders-to-pick">
 				<a href="<?php echo esc_url( admin_url( 'edit.php?post_status=wc-processing&post_type=shop_order' ) ); ?>">
 				<?php
 					printf(
-						_n( '<strong>%s order</strong> awaiting processing', '<strong>%s orders</strong> awaiting processing', $processing_count, 'woocommerce' ),
-						$processing_count
+						_n( '<strong>%s order</strong> awaiting processing', '<strong>%s orders</strong> awaiting processing', $orders_to_pick, 'woocommerce' ),
+						$orders_to_pick
 					);
 				?>
 				</a>
 			</li>
-			<li class="on-hold-orders">
+			<li class="orders-to-supply">
 				<a href="<?php echo esc_url( admin_url( 'edit.php?post_status=wc-on-hold&post_type=shop_order' ) ); ?>">
 				<?php
 					printf(
-						_n( '<strong>%s order</strong> on-hold', '<strong>%s orders</strong> on-hold', $on_hold_count, 'woocommerce' ),
-						$on_hold_count
+						_n( '<strong>%s order</strong> on-hold', '<strong>%s orders</strong> on-hold', $orders_to_supply, 'woocommerce' ),
+						$orders_to_supply
 					);
 				?>
 				</a>
